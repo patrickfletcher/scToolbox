ee5664455459e53af6f19effa5998da7fafa9d8f
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2020-03-16 16:17:30 -0400

    doUMAP - directly use python umap-learn

--- doUMAP.m
+++ doUMAP.m
@@ -3,36 +3,55 @@ function result = doUMAP(score, params, figID, valuenames, colors)
 disp('Computing UMAP...')
 result = params;
 
+Pypath = py.sys.path;
+MLpath=string(path).split(';');
+sctoolpath=MLpath(contains(MLpath,'scToolbox'));
+if count(Pypath,sctoolpath) == 0 && ~isempty(sctoolpath)
+    insert(Pypath,int32(0),sctoolpath);
+end
+
+umap=py.importlib.import_module('umap');
+
 rng(params.rngSeed)
 
-if isempty(params.initY)
-    initY=[];
+initY=params.initY;
+if isempty(initY)
+    initY="spectral";
 
-elseif isnumeric(params.initY)&&numel(params.initY)==2
+elseif isnumeric(initY)&&numel(initY)==2
     %initY=[pc1,pc2] indicates index of PCs to use as initial points
 
-    pcix=params.initY;
+    pcix=initY;
     signix=sign(pcix);
     pcix=abs(pcix);
     initY=score(:,pcix);
     initY(:,1)=signix(1)*initY(:,1);
     initY(:,2)=signix(2)*initY(:,2);
-    
-%         initY=score(:,params.initY);
-%         initY=initY+ 5*(rand(size(initY))-0.5); %random jitter?
+    initY=py.numpy.array(initY);
 
-elseif size(params.initY,1)==size(score,1)
-    initY=params.initY;
+elseif size(initY,1)==size(score,1)
+    initY=py.numpy.array(initY);
 
 end
 
 %non-standard for run_umap api: 'init', 'spread'
 % 'init',initY, 'spread',params.spread, ...    
 
-res=run_umap(score,'n_neighbors',params.n_neighbors,...
-    'n_epochs',params.n_epochs,'verbose','text','min_dist',params.min_dist,...
-    'randomize',params.randomize,'method',params.method,...
-    'python',params.python);
+% res=run_umap(score,'n_neighbors',params.n_neighbors,...
+%     'n_epochs',params.n_epochs,'verbose','text','min_dist',params.min_dist,...
+%     'randomize',params.randomize,'method',params.method,...
+%     'python',params.python);
+
+% kwargs =py.dict(pyargs('n_neighbors',int32(params.n_neighbors),'min_dist',params.min_dist,...
+%                 'n_epochs',int32(params.n_epochs),'verbose','text',...
+%                 'n_components',int32(2),'verbose',true,'init',initY));
+% res=py.runumap.run(score,kwargs);
+
+kwargs = pyargs('n_neighbors',int32(params.n_neighbors),'min_dist',params.min_dist,...
+                'n_epochs',int32(params.n_epochs),'verbose','text',...
+                'n_components',int32(2),'verbose',true,'init',initY);
+res=umap.UMAP(kwargs).fit_transform(score);
+res=double(res);
 
 result.coords=res;
 
