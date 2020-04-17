da39a481e2179c7d68190b6896f94ea623b3b8fc
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2020-04-17 00:11:33 -0400

    add multiple-axis colorby="group" support

--- plotScatter.m
+++ plotScatter.m
@@ -1,4 +1,4 @@
-function [ax, hs, hc]=plotScatter(X,colorby,group,colors,figID,subplotdims,sp_params,docolorbar)
+function [ax, hs, hc]=plotScatter(X,colorby,groups,colors,figID,subplotdims,sp_params,docolorbar)
 %scatter plot of points in X, colored by either category or values
 % X - data, rows are points, colums dimension (2 or 3 only)
 % colorby - {'group','value'}.
@@ -13,9 +13,8 @@ function [ax, hs, hc]=plotScatter(X,colorby,group,colors,figID,subplotdims,sp_pa
 %TODO: mouse-over function: make it display the type name or expr value
 
 %TODO: pass in args to control marker size etc
-marker='o';
-
 nObs=size(X,1);
+marker='o';
 markerSize=11-log(nObs);
 
 %common setup
@@ -43,8 +42,56 @@ else
     figure(figID);
 end
 
-if ~exist('docolorbar','var')||isempty(docolorbar)
-    docolorbar=true;
+%handle subplotting...
+if colorby=="group"
+    %group: categorical/logical/integer matrix, or cell array if >1 plots
+    %colors: color matrix, one row per type. (or cell array of these)
+    if iscell(groups)
+        nPlots=length(groups);
+    else
+        [m,n]=size(groups); %should be single row/col vector length nObs
+        if m~=nObs && n~=nObs
+            error('Number of observations does not match number of scatter points')
+        end
+        nPlots=1;
+        groups={groups};
+        if ~iscell(colors)
+            colors={colors};
+        end
+    end
+elseif colorby=="value"
+    %group - simply labels for the plots...
+    %colors - size(X,1) by number of variables to plot
+    [m,n]=size(colors);
+    if m~=nObs && n~=nObs
+        error('Number of observations does not match number of scatter points')
+    elseif n==size(X,1) 
+        colors=colors'; %force columns
+        [~,n]=size(colors);
+    end
+    nPlots=n;
+end
+
+mustMakeAxes=true;
+if exist('subplotdims','var')&&~isempty(subplotdims)
+    if class(subplotdims)=="matlab.graphics.axis.Axes"
+        ax=subplotdims; %could be an array of axes
+        if length(ax)==nPlots
+            mustMakeAxes=false;
+        end
+    elseif isnumeric(subplotdims)
+        if prod(subplotdims)==nPlots
+            nr=subplotdims(1);
+            nc=subplotdims(2);
+            mustMakeAxes=false;
+        end
+    end
+end
+if mustMakeAxes
+    p=numSubplots(nPlots);
+    nr=p(1);
+    nc=p(2);
+    ax=tight_subplot(nr,nc,[],sp_params.gap,sp_params.marg_h,sp_params.marg_w);
 end
 
 
@@ -52,59 +99,55 @@ switch lower(colorby)
     
     case 'group'
         
-        if isempty(group)
-            group=ones(size(X,1),1);
-        end
-        
-        if ~iscategorical(group)
-            group=categorical(group);
-        end
-        groupNames=categories(group);
-        groupCounts=countcats(group);
-        
-        if ~exist('colors','var')||isempty(colors)||size(colors,1)<length(groupNames)
-            colors=cbrewer('qual','Set1',max(length(groupNames),3));
-        end
-        
-        colors=colors(groupCounts>0,:);
-        group=removecats(group);
-        groupNames=categories(group);
+        for i=1:nPlots
+            axes(ax(i));
             
-        if ~exist('subplotdims','var')||isempty(subplotdims)||~(class(subplotdims)=="matlab.graphics.axis.Axes")
-%             ax=subplot(1,1,1);
-            ax=tight_subplot(1,1,1,sp_params.gap,sp_params.marg_h,sp_params.marg_w);
-        else
-            ax=subplotdims;
-            axes(ax); %bring specified axes into focus
-        end
-        
-%         hs=gscatter(X(:,1),X(:,2),group);
-%         for i=1:length(hs)
-%             hs(i).Marker='o';
-%             hs(i).MarkerFaceColor=colors(i,:);
-%             hs(i).MarkerEdgeColor=colors(i,:)*0.66; %darker edge of same color
-%             hs(i).MarkerSize=4; 
-%             hs(i).ZData=rand(size(hs(i).XData));  %randomize the "depth" of points
-%         end
+            group=groups{i};
+            color=colors{i};
+            
+            if isempty(group)
+                group=ones(size(X,1),1);
+            end
 
-    %alt using basic scatter only
-        hold on
-        for i=1:length(groupNames)
-            thisGroup=group==groupNames{i};
-            hs(i)=scatter(X(thisGroup,1),X(thisGroup,2),markerSize,colors(i,:),marker,'filled');
-            hs(i).MarkerEdgeColor=colors(i,:)*0.66; %darker edge of same color
-            hs(i).ZData=rand(size(hs(i).XData));  %randomize the "depth" of points
-            hs(i).DisplayName=groupNames{i};
-        end
-        hold off
+            if ~iscategorical(group)
+                group=categorical(group);
+            end
+            groupNames=categories(group);
+            groupCounts=countcats(group);
 
-        ax.SortMethod='depth';
-        
-        legend off %create user legend outside
-%         axis tight
-        axis off
-        
-        hc=[];
+            autoColors=true;
+            if ~isempty(color)
+                if size(color,1)==length(groupNames)
+                    color=color(groupCounts>0,:);
+                    autoColors=false;
+                end
+            end
+            
+            group=removecats(group);
+            groupNames=categories(group);
+            
+            if autoColors
+                color=cbrewer('qual','Set1',max(length(groupNames),3));
+            end
+            
+            hold on
+            for j=1:length(groupNames)
+                thisGroup=group==groupNames{j};
+                hs(j)=scatter(X(thisGroup,1),X(thisGroup,2),markerSize,color(j,:),marker,'filled');
+                hs(j).MarkerEdgeColor=color(j,:)*0.66; %darker edge of same color
+                hs(j).ZData=rand(size(hs(j).XData));  %randomize the "depth" of points
+                hs(j).DisplayName=groupNames{j};
+            end
+            hold off
+
+            ax(i).SortMethod='depth';
+
+            legend off %create user legend outside
+    %         axis tight
+            axis off
+
+            hc=[];
+        end
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     case 'value'
@@ -113,45 +156,11 @@ switch lower(colorby)
         cmap=cbrewer('seq','OrRd',17); %test binning?
         cmap=[[.7,.85,0.9];cmap(4:end,:)]; %add a teal color for zero; skip lower couple colors
 
-        %group - simply labels for the plots...
-        %colors - size(X,1) by number of variables to plot
-        
-        [m,n]=size(colors);
-        if m~=nObs && n~=nObs
-            error('Number of observations does not match number of scatter points')
-        elseif n==size(X,1) 
-            colors=colors'; %force columns
-            [~,n]=size(colors);
-        end
-        nVars=n;
-        
-        if isempty(group)
-            group=repmat("",n,1);
+        if isempty(groups)
+            groups=repmat("",n,1);
         end
         
-        mustMakeAxes=true;
-        if ~exist('subplotdims','var')||isempty(subplotdims)
-            p=numSubplots(nVars);
-            nr=p(1);
-            nc=p(2);
-        elseif class(subplotdims)=="matlab.graphics.axis.Axes"
-            mustMakeAxes=false;
-            ax=subplotdims; %could be an array of axes
-        elseif isnumeric(subplotdims)
-            if prod(subplotdims)>=nVars
-                nr=subplotdims(1);
-                nc=subplotdims(2);
-            else
-                p=numSubplots(nVars);
-                nr=p(1);
-                nc=p(2);
-            end
-        end
-        if mustMakeAxes
-            ax=tight_subplot(nr,nc,[],sp_params.gap,sp_params.marg_h,sp_params.marg_w);
-        end
-        
-        for i=1:nVars
+        for i=1:nPlots
             axes(ax(i));
             
             hs(i)=scatter(X(:,1),X(:,2),markerSize,colors(:,i),marker,'filled');
@@ -164,6 +173,10 @@ switch lower(colorby)
             colormap(cmap);
             
             %custom colobar, small & centered to the right. shows only max/min color
+
+            if ~exist('docolorbar','var')||isempty(docolorbar)
+                docolorbar=true;
+            end
             if docolorbar
                 c=colorbar;
                 c.Position(1)=ax(i).Position(1)+ax(i).Position(3)+cb_gap;
@@ -171,19 +184,20 @@ switch lower(colorby)
                 c.Position(2)=c.Position(2)+c.Position(4)/4;
                 c.Position(4)=c.Position(4)/2;
 
-                c.Ticks=[ceil(10*c.Limits(1)),floor(10*c.Limits(2))]/10; %round to 1 decimal point
+                d=2; 
+                c.Ticks=[ceil((10^d)*c.Limits(1)),floor((10^d)*c.Limits(2))]/ (10^d); %round to 1 decimal point
 
                 hc(i)=c;
             else
                 hc=[];
             end
             
-            hs(i).ZData=rand(size(hs(i).XData));  %randomize the "depth" of points
+            hs(i).ZData=rand(size(hs(i).XData))*0.01;  %randomize the "depth" of points
 %             hs(i).ZData=colors(:,i); %high expr on top
             
             ax(i).SortMethod='depth';
             
-            title(group(i));
+            title(groups(i));
             
         end
         
