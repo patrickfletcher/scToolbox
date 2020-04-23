e09c77eab152968640c3079ee6a23d02cd8a48b6
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2020-04-23 08:54:05 -0400

    begin "UpSet"-like set intersection plot

--- compareGeneLists.m
+++ compareGeneLists.m
@@ -4,3 +4,9 @@ genes2(strcmp(genes2,''))=[];
 [unique1,iu1]=setdiff(genes1,genes2);
 [unique2,iu2]=setdiff(genes2,genes1);
 [common,ic1,ic2]=intersect(genes1,genes2);
+
+
+
+% [~,iu1,iu2]=setxor(genes1,genes2);
+% unique1=genes1(iu1);
+% unique2=genes2(iu2);
---
+++ upsetplot.m
@@ -0,0 +1,17 @@
+classdef upsetplot
+    %for set intersection visuals of >3 sets (or alternative to venn for 3)
+    
+    %make something like:https://github.com/ImSoErgodic/py-upset, https://github.com/hms-dbmi/UpSetR
+    
+    properties
+        %make separate classes containing each plot
+        ax_intersect %bar showing size of intersections
+        ax_combs %line/dot plot showing set combination for each bar
+        ax_setcount %bar showing size of each set
+    end
+    
+    methods 
+        function hup=upsetplot()
+        end
+    end
+end
