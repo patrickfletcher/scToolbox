62d0a96a2b75d929008bbab8ee001fa931200112
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2020-05-05 12:22:22 -0400

    Update getExpression.m

--- getExpression.m
+++ getExpression.m
@@ -3,10 +3,17 @@ function geneTable=getExpression(genes,ncounts,tcounts,factor1,factor2,threshgro
 %
 % assumes tcounts is already threshold-subtracted, unless a threshold-group
 % is passed in...
+
+%TODO: use stratify_factors
+
+%remove cats first?
+factor1=removecats(factor1);
 factor1Names=categories(factor1);
 
 do2factor=false;
 if exist('factor2','var')&&~isempty(factor2)
+    %remove cats first?
+    factor2=removecats(factor2);
     factor2Names=categories(factor2);
     do2factor=true;
 end
@@ -28,23 +35,22 @@ if doThreshold
     end
 end
 
-geneTable=table();
+geneExpr=table();
+genePrct=table();
 for i=1:length(factor1Names)
     if do2factor
         for j=1:length(factor2Names)
             thisName=[factor1Names{i},'_',factor2Names{j}];
             thisGroup=factor1==factor1Names(i) & factor2==factor2Names{j};
-            geneTable.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
-            geneTable.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
+            genePrct.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
+            geneExpr.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
         end
     else
         thisName=factor1Names{i};
         thisGroup=factor1==factor1Names(i);
-        geneTable.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
-        geneTable.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
+        genePrct.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
+        geneExpr.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
     end
 end
 
-%reorder the columns in alphabetical order
-geneTable=geneTable(:,sort(geneTable.Properties.VariableNames));
-geneTable=[genes(:,{'id','name'}),geneTable];
+geneTable=[genes(:,{'id','name'}),geneExpr,genePrct];
