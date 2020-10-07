bfa226bd98b044b0e0493063c383cfa5d7bbf18d
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2020-10-07 19:22:08 -0400

    Create set_embedding_textpos.m

---
+++ set_embedding_textpos.m
@@ -0,0 +1,28 @@
+function [textpos,catnames] = set_embedding_textpos(COORDS, ident, cols, textlab)
+
+catnames=categories(ident);
+ctcounts=countcats(ident);
+catnames(ctcounts==0)=[];
+catnames(catnames=="Amb")=[];
+textpos=zeros(length(catnames),2);
+for i=1:length(catnames)
+    textpos(i,:)=mean(COORDS(ident==catnames{i},:));
+end
+textpos=[textpos,ones(length(catnames),2)];
+
+if ~exist('textlab','var') 
+    textlab=catnames;
+end
+
+fh=figure();clf
+ax=plotScatter(COORDS, 'group', ident, cols, fh);
+
+ht1=text(ax,textpos(:,1),textpos(:,2),ones(size(textpos,1),1),textlab,...
+    'HorizontalAlignment','center');
+drawnow
+
+%pause to adjust label positions
+pause
+
+%extract the new positions after the pause
+textpos=cat(1,ht1(:).Position);
