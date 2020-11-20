b0607aedb3da06c229b4fa03ed84b83f2ea4f6b7
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2020-11-20 13:06:20 -0500

    Update get_exclusive_intersections.m

--- get_exclusive_intersections.m
+++ get_exclusive_intersections.m
@@ -39,3 +39,37 @@ for k=1:n_sets
         intersection_ix(end+1)={this_in_ix};
     end
 end
+
+%
+
+% % sets=varargin;
+% n_sets=length(sets);
+% 
+% % n_exclusive_intersections=0;
+% % for k=1:n_sets
+% %     n_exclusive_intersections=n_exclusive_intersections+nchoosek(n_sets,k);
+% % end
+% 
+% set_ix_list=1:n_sets;
+% % exclusive_intersections={};
+% intersection_ix={};
+% for k=1:n_sets
+%     in_ix=combnk(set_ix_list,k);
+%     for i=1:size(in_ix,1)
+%         %compute the intersection of in_sets
+%         this_in_ix=in_ix(i,:);
+%         this_excl_int=sets{in_ix(i,1)};
+%         for j=2:length(this_in_ix)
+%             this_excl_int=intersect(this_excl_int, sets{this_in_ix(j)});
+%         end
+%         %subtract away the other (out) sets
+%         out_ix=setdiff(set_ix_list, this_in_ix);
+%         for j=1:length(out_ix)
+%             this_excl_int=setdiff(this_excl_int, sets{out_ix(j)});
+%         end
+% %         exclusive_intersections(end+1)={this_excl_int};
+%         intersection_ix(end+1)={this_in_ix};
+%         this_fieldname=strjoin(setnames(this_in_ix),"_");
+%         exclusive_intersections.(this_fieldname)=this_excl_int;
+%     end
+% end
