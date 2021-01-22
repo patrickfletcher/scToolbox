a8f8a48986edada86df8367778bd4212df556e30
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2021-01-22 09:29:29 -0500

    Update violinmatrix.m

--- violinmatrix.m
+++ violinmatrix.m
@@ -1,5 +1,8 @@
-function [v,ax,ylh,labs]=violinmatrix(data,group,groupColors,varNames,gap,margins)
-% data has rows=observations, columns=variables
+function [v,ax,ylh]=violinmatrix(data,group,groupColors,varNames,bandwidth,sppars)
+
+%thr=scalar: one line across all (gene thresh)
+%thr=matrix 2x size nGroups: per violin hi/lo?
+% thrw=0.33;
 
 if ~iscategorical(group)
     group=categorical(group);
@@ -12,42 +15,51 @@ if isempty(groupColors)
     groupColors=cbrewer('qual','Set1',nGroups);
 end
 
-ylh=[];
-[~,pos]=tight_subplot(nVars,1,gap,margins(1:2),margins(3:4));
-% POS=cat(1,pos{:});
+if ~exist('bandwidth','var')
+    bandwidth=0.1;
+end
+
+if ~exist('sppars','var')
+    sppars.gap=0.05;
+    sppars.marg_h=0.05;
+    sppars.marg_w=0.05;
+end
+
+% t=tiledlayout(nVars,1);
+% t.Padding='compact';
+% t.TileSpacing='none';
 for i=1:nVars
-    ax(i)=subplot('Position',pos{i});
+%     ax(i)=nexttile(t);
+    ax(i)=tight_subplot(nVars,1,i,sppars.gap,sppars.marg_h,sppars.marg_w);
     ax(i).YTickLabelMode='auto';
     
-%     ax(i)=subplot_tight(nVars,1,i,spmargins);
-    v(i,:)=violinplot(data(:,i),group,'BandWidth',0.1);
-%     ylh(i)=ylabel(varNames{i});
-    
+    ylh(i)=ylabel(varNames{i});
 %     if mod(i,2)==0
 %         ax(i).YAxisLocation='right';
 %     end
-
+    axis tight
     xlim(ax(i),[0.5,nGroups+0.5]);
-    ax(i).YAxisLocation='right';
+    if i<nVars
+%         ax(i).XTick=[];
+        ax(i).XAxis.Visible='off';
+    end
     
+    v(i,:)=violinplot(data(i,:),group,'BandWidth',bandwidth);
     for j=1:nGroups
         v(i,j).ViolinColor=groupColors(j,:);
-        v(i,j).ViolinAlpha=0.8;
+        v(i,j).ViolinAlpha=1;
         v(i,j).ShowData=false;
         v(i,j).MedianPlot.Visible='off';
         v(i,j).BoxPlot.Visible='off';
         v(i,j).WhiskerPlot.Visible='off';
-        v(i,j).MedianPlot.Visible='off';
     end
     
-%     grid on
-    if i<nVars
-        ax(i).XAxis.Visible='off';
-    end
-    
-    XLIM=xlim();
-    YLIM=ylim();
-    labs(i)=text(XLIM(1)-0.05,YLIM(1)+diff(YLIM)/2,varNames(i),'HorizontalAlignment','right','VerticalAlignment','middle');
+%     if ~isempty(thr) && isscalar(thr(i,:))
+%         line(ax(i),xlim(ax(i)),[1,1]*thr(i),'color',[0.7,0.7,0.7])
+%     else
+%         for j=1:nGroups
+%             line(ax(i),j+thrw*[-1,1],[1,1]*thr(i),'color',[0.7,0.7,0.7])
+%         end
+%     end
 end
-% labs=text(POS(:,1),POS(:,2),varNames,'Units','Normalized','HorizontalAlignment','right','VerticalAlignment','top');
-% linkaxes(ax,'y')
+ax(end).XTickLabel=groupnames;
