347b83567f388304b715b3edcecc3b4258e25071
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2021-06-15 22:42:09 -0400

    add multiple methods for computing center value of expression

--- getExpression.m
+++ getExpression.m
@@ -1,40 +1,47 @@
-function geneTable=getExpression(genes,ncounts,tcounts,factor1,factor2,threshgroup)
-%geneTable=getExpression(genes,ncounts,tcounts,factor1,factor2,threshgroup)
-%
+function geneTable=getExpression(genes,ncounts,tcounts,factor,options)
+arguments
+    genes
+    ncounts
+    tcounts
+    factor
+    options.factor2 = []
+    options.threshgroup = []
+    options.method = 'mean'
+    options.trim_prct = 0
+end
 %get gene expression for groups defined by two levels of factors
 %
 % assumes tcounts is already threshold-subtracted, unless a threshold-group
 % is passed in...
 
-%TODO: use stratify_factors
+%TODO: remove second factor?
+%TODO: option for either mean/prct?
+%TODO: use accumarray/groupcounts
 
 %remove cats first?
-if ~iscategorical(factor1)
-    factor1=categorical(factor1);
+if ~iscategorical(factor)
+    factor=categorical(factor);
 end
-factor1=removecats(factor1);
-factor1Names=categories(factor1);
+factor=removecats(factor);
+factor1Names=categories(factor);
 
 do2factor=false;
-if exist('factor2','var')&&~isempty(factor2)
+if ~isempty(options.factor2)
     %remove cats first?
-    if ~iscategorical(factor2)
-        factor2=categorical(factor2);
-    end
+    factor2=categorical(options.factor2);
     factor2=removecats(factor2);
-    factor2Names=categories(factor2);
     do2factor=true;
 end
 
 doThreshold=false;
-if exist('threshgroup','var')
-    if isscalar(threshgroup)
-        if threshgroup==1 %will use genes.thr
+if ~isempty(options.threshgroup)
+    if isscalar(options.threshgroup)
+        if options.threshgroup==1 %will use genes.thr
             threshgroups=1;
             doThreshold=true;
         end
-    elseif length(threshgroup)==size(tcounts,2)
-        threshgroup=categorical(threshgroup);
+    elseif length(options.threshgroup)==size(tcounts,2) %should really compute thr here, not expect a column in genes
+        threshgroup=categorical(options.threshgroup);
         threshgroups=categories(threshgroup);
         doThreshold=true;
     end
@@ -51,22 +58,59 @@ if doThreshold
     end
 end
 
+
+if do2factor
+    factor = stratify_factors(factor, factor2);
+    factor = removecats(factor);
+end
+factorNames = categories(factor);
+
 geneExpr=table();
 genePrct=table();
-for i=1:length(factor1Names)
-    if do2factor
-        for j=1:length(factor2Names)
-            thisName=[factor1Names{i},'_',factor2Names{j}];
-            thisGroup=factor1==factor1Names(i) & factor2==factor2Names{j};
-            genePrct.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
-            geneExpr.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
-        end
-    else
-        thisName=factor1Names{i};
-        thisGroup=factor1==factor1Names(i);
-        genePrct.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
-        geneExpr.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
+for i=1:length(factorNames)
+    thisName=factorNames{i};
+    thisGroup=factor==factorNames(i);
+    genePrct.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
+    switch options.method
+        case 'mean'
+            thisExpr=mean(ncounts(:,thisGroup),2);
+        case 'median'
+            thisExpr=median(ncounts(:,thisGroup),2);
+        case 'geomean'
+            thisExpr=geomean(ncounts(:,thisGroup),2);
+        case 'harmmean'
+            thisExpr=harmmean(ncounts(:,thisGroup),2);
+        case 'trimean'
+            thisExpr=mean(prctile(ncounts(:,thisGroup),[25,50,50,75],2),2);
+        case 'trimmean'
+            thisExpr=trimmean(ncounts(:,thisGroup),options.trim_prct,2);
     end
+    geneExpr.(['expr_',thisName]) = thisExpr;
 end
 
 geneTable=[genes(:,{'id','name'}),geneExpr,genePrct];
+
+
+% for i=1:length(factor1Names)
+%     if do2factor
+%         for j=1:length(factor2Names)
+%             thisName=[factor1Names{i},'_',factor2Names{j}];
+%             thisGroup=factor1==factor1Names(i) & factor2==factor2Names{j};
+%             genePrct.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
+%             geneExpr.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
+%         end
+%     else
+%         thisName=factor1Names{i};
+%         thisGroup=factor1==factor1Names(i);
+%         genePrct.(['prct_',thisName])=sum(tcounts(:,thisGroup)>0,2)./nnz(thisGroup)*100;
+%         switch method
+%             case 'mean'
+%         geneExpr.(['expr_',thisName])=mean(ncounts(:,thisGroup),2);
+%             case 'median'
+%             case 'geomean'
+%             case 'harmmean'
+%             case 'trimean'
+%             case 'trimmean'
+%         end
+%     end
+% end
