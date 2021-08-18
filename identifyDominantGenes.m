4f34b6927540cd0d7da4e74f9b5df08c58198169
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2021-08-18 09:47:50 -0400

    Update identifyDominantGenes.m
    - fix other test-pooling bug
    - can bypass use of P-values
    - add "some" option for self pooling

--- identifyDominantGenes.m
+++ identifyDominantGenes.m
@@ -11,26 +11,31 @@ function [dominant,specific,allgenes,dominantCondition, specificCondition]...
 %TODO: alternate interface - tcounts+categorical; compute the mean expr &
 %prct expressing here. Also, compute P if not passed in...
 
+%TODO: use arguments block
+
+%TODO: toggle which ES to use (or combo)
+
 %parameters
 [self, other, params]=parse_input_pars(selfin,otherin, paramsin);
 
 nGenes=height(genes);
 
-%TODO: switch to use just one test or other
-
-useP=false;
+% whole group quantities, tests: P.anova, P.chi2
 Panova=zeros(nGenes,1);
 Pchi2=zeros(nGenes,1);
-if ~isempty(P)
-    useP=true;
-    % whole group quantities, tests: P.anova, P.chi2
-    if isfield(P,'anova')
-        Panova = P.anova;
-    end
-    if isfield(P,'chi2')
-        Pchi2 = P.chi2;
-    end
+
+%TODO: switch is based on presence of anova/chi2. probably a better way
+usePexpr=false;
+if ~isempty(P) && isfield(P,'anova')
+    Panova = P.anova;
+    usePexpr=true;
 end
+usePprop=false;
+if ~isempty(P) && isfield(P,'chi2')
+    Pchi2 = P.chi2;
+    usePprop=true;
+end
+    
 test_pAnova=Panova < params.pthr;
 test_pChi2=Pchi2 < params.pprothr;
 
@@ -50,7 +55,7 @@ test_otherMaxPrct=prct_other<other.maxprct;  %for up SPEC: all vs pairwise?
 test_selfMaxPrct=prct_self<self.maxprct; %for down
 test_otherMinPrct=prct_other>other.minprct;
 
-%bug with self.poolmethod='any': need to apply the minPrct & specific tests
+%bug with self.poolmethod='any'/'some': need to apply the minPrct & specific tests
 %per self.name in that case
 switch params.direction
     case 'up'
@@ -118,15 +123,18 @@ for i=1:length(self.names)
     pairwise_exprES=[pairwise_exprES,this_exprES];
     pairwise_prctES=[pairwise_prctES,this_prctES];
     
-    if useP
-        %Pairwise p-values. loop only to get otherNameInCombo
+    %Pairwise p-values. in loop because need otherNameInCombo
+    if usePexpr||usePprop
         selfNameInCombo=any(strcmp(P.combs,self.names{i}),2);
         otherNameInCombo=any(ismember(P.combs,other.names),2);
+    end
+    this_pmc=zeros(size(this_exprES)); %makes P-val tests always true
+    if usePexpr
         this_pmc=P.mc(:,selfNameInCombo&otherNameInCombo);
+    end
+    this_pz=zeros(size(this_prctES));
+    if usePprop
         this_pz=P.z(:,selfNameInCombo&otherNameInCombo);
-    else
-        this_pmc=zeros(size(this_exprES)); %makes P-val tests always true
-        this_pz=zeros(size(this_prctES));
     end
     
     pairwise_Pmc=[pairwise_Pmc, this_pmc];
@@ -139,28 +147,23 @@ for i=1:length(self.names)
         case 'up'
             test_thisExpr_ES=this_exprES>params.fcExprThr;
             test_thisPrct_ES=this_prctES>params.prctESThr;
-            test_pairwise_ESexpr_limit=this_exprES>params.minFcExpr; %lower bound for ES
+            test_pairwise_ESexpr_limit=this_exprES>params.minFcEffect; %lower bound for ES
             test_pairwise_ESprct_limit=this_prctES>params.minPrctEffect;
         case 'down'
             test_thisExpr_ES=this_exprES<1/params.fcExprThr;
             test_thisPrct_ES=this_prctES<-params.prctESThr;
-            test_pairwise_ESexpr_limit=this_exprES<1/params.minFcExpr;
+            test_pairwise_ESexpr_limit=this_exprES<1/params.minFcEffect;
             test_pairwise_ESprct_limit=this_prctES<-params.minPrctEffect;
         case 'both'
             test_thisExpr_ES=this_exprES>params.fcExprThr | this_exprES<1/params.fcExprThr;
             test_thisPrct_ES=this_prctES>params.prctESThr | this_prctES<-params.prctESThr;
-            test_pairwise_ESexpr_limit=this_exprES>params.minFcExpr | this_exprES<1/params.minFcExpr;
+            test_pairwise_ESexpr_limit=this_exprES>params.minFcEffect | this_exprES<1/params.minFcEffect;
             test_pairwise_ESprct_limit=this_prctES>params.minPrctEffect | this_prctES<-params.minPrctEffect;
     end
 
-    
     % combine pairwise P value tests and effect-size tests: significant pairwise ES tests 
     pairwise_Expr= test_thisExpr_P & test_thisExpr_ES;
     pairwise_Prct= test_thisPrct_P & test_thisPrct_ES;
-         
-%     % self% and other% filters
-%     pairwise_Expr=pairwise_Expr & test_thisSelfPrct;
-%     pairwise_Prct=pairwise_Prct & test_thisSelfPrct;
     
     %limiters are to force constraint on the other ES test
     if params.limitMode=="pairwise"
@@ -175,35 +178,39 @@ for i=1:length(self.names)
 %         pairwise_Prct_spec=pairwise_Prct_spec & test_thisSpecific;
 %     end
     
-    % combine the two ES tests
-    switch params.combine_prct_expr
-        case 'or'
-            %marks dominant if some tests pass only by expr, others only by
-            %prct such that neither all(expr tests) nor all(prct test).
-            pairwise_combinedES=pairwise_Expr|pairwise_Prct; %permits either test
-%             pairwise_combinedES_spec=pairwise_Expr_spec|pairwise_Prct_spec;
-        case 'and'
-            pairwise_combinedES=pairwise_Expr&pairwise_Prct; %strict
-%             pairwise_combinedES_spec=pairwise_Expr_spec&pairwise_Prct_spec; 
-    end
     
-    %NOW: combine all the pairwise tests. ALL by default.
+    %combine all the pairwise tests. ALL by default.
     switch other.poolmethod
         case "all"
-            dominant_otherpooled(:,i)=all(pairwise_combinedES,2);
+%             dominant_otherpooled(:,i)=all(pairwise_combinedES,2);
 %             specific_otherpooled(:,i)=all(pairwise_combinedES_spec,2);
             self_test_exprES(:,i)=all(pairwise_Expr,2);
             self_test_prctES(:,i)=all(pairwise_Prct,2);
         case "any"
-            dominant_otherpooled(:,i)=any(pairwise_combinedES,2);
+%             dominant_otherpooled(:,i)=any(pairwise_combinedES,2);
 %             specific_otherpooled(:,i)=any(pairwise_combinedES_spec,2);
             self_test_exprES(:,i)=any(pairwise_Expr,2);
             self_test_prctES(:,i)=any(pairwise_Prct,2);
         case "some"
-            dominant_otherpooled(:,i)=sum(pairwise_combinedES,2)>other.some_N;
+%             dominant_otherpooled(:,i)=sum(pairwise_combinedES,2)>other.some_N;
 %             specific_otherpooled(:,i)=sum(pairwise_combinedES_spec,2)>other.some_N;
             self_test_exprES(:,i)=sum(pairwise_Expr,2)>other.some_N;
             self_test_prctES(:,i)=sum(pairwise_Prct,2)>other.some_N;
+    end
+    
+    % combine the two ES tests. After pooling others (e.g. all exprtest OR
+    % all prcttest - not a mixture of individual pairs...)
+    switch params.combine_prct_expr
+        case 'or'
+            %marks dominant if some tests pass only by expr, others only by
+            %prct such that neither all(expr tests) nor all(prct test).
+%             pairwise_combinedES=pairwise_Expr|pairwise_Prct; %permits either test
+%             pairwise_combinedES_spec=pairwise_Expr_spec|pairwise_Prct_spec;
+            dominant_otherpooled(:,i)=self_test_exprES(:,i)|self_test_prctES(:,i);
+        case 'and'
+%             pairwise_combinedES=pairwise_Expr&pairwise_Prct; %strict
+%             pairwise_combinedES_spec=pairwise_Expr_spec&pairwise_Prct_spec; 
+            dominant_otherpooled(:,i)=self_test_exprES(:,i)&self_test_prctES(:,i);
     end
 end
 
@@ -233,8 +240,11 @@ switch self.poolmethod
         specific_selfpooled=any(specific_otherpooled,2);
         exprTestPooled=any(self_test_exprES,2);
         prctTestPooled=any(self_test_prctES,2);
-%     case "some" 
-%       something like "findMarkers" in scran?
+    case "some" 
+        dominant_selfpooled=sum(dominant_otherpooled,2)>self.some_N;
+        specific_selfpooled=sum(specific_otherpooled,2)>self.some_N;
+        exprTestPooled=any(self_test_exprES,2);
+        prctTestPooled=any(self_test_prctES,2);
 end
 
 
@@ -336,9 +346,9 @@ params.combine_prct_expr='or';
 params.specificMode='uniform'; %or pairwise
 params.prctMetric='diff';
 params.prctESThr=20;
-params.minPrctEffect=0;
 params.fcExprThr=1;
-params.minFcExpr=0;
+params.minPrctEffect=-100;
+params.minFcEffect=0;
 params.pthr=0.1;
 params.pthrpw=0.1;
 params.pprothr=0.1;
