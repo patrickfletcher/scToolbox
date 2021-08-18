1583fa3c244552d91e21234cebb775ad0b68580e
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2021-08-18 09:44:50 -0400

    begin goatools python interface

--- goatools.m
+++ goatools.m
@@ -1,10 +1,20 @@
 classdef goatools < handle
-%simple wrapper class for some python goatools utilities
+    %simple wrapper class for some python goatools functionality
     
-properties
-end
-
-methods
-end
-
+    properties
+        dag %the whole OBO graph datastructure
+    end
+    
+    methods
+        %constructor
+        function gt = goatools()
+            arguments
+                
+            end
+            GODagargs=pyargs('optional_attrs','relationship');
+            gt.dag=py.goatools.obo_parser.GODag(GODagargs);
+            
+        end
+    end
+    
 end
