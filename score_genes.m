0f4bfd251b8a4f84ae697738192b4f40ca7a6991
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2021-10-06 12:18:45 -0400

    Update score_genes.m

--- score_genes.m
+++ score_genes.m
@@ -1,21 +1,17 @@
-function score = score_genes(query_genes, expr, gene_pool, ctrl_size, n_bins, min_cells, rng_state)
+function score = score_genes(query_genes, expr, gene_pool, options)
+arguments
+    query_genes
+    expr
+    gene_pool
+    options.ctrl_size = 50
+    options.n_bins = 25
+    options.min_cells = 1  %TODO - this is unused
+    options.rng_seed = 42
+end
 % compute gene scores as done by AddModuleScore in Seurat and
 % scanpy.tl.score_genes: Tirosh2016 DOI: 10.1126/science.aad0501
 
-if ~exist('ctrl_size','var')||isempty(ctrl_size)
-    ctrl_size=50;
-end
-if ~exist('n_bins','var')||isempty(n_bins)
-    n_bins=25;
-end
-if ~exist('min_cells','var')||isempty(min_cells)
-    min_cells=1;
-end
-if ~exist('rng_state','var')||isempty(rng_state)
-    rng_state=42;
-end
-
-rng(rng_state)
+rng(options.rng_seed)
 
 % cellsExpr=sum(expr>0,2);
 % expr=expr(cellsExpr>=min_cells,:);
@@ -25,7 +21,7 @@ meanExpr=mean(expr,2);
 [sortedMean,ixs]=sort(meanExpr); %genebin_ids should index into sorted ixs
 
 %compute background scores in each expression bin
-bin_edges=round(linspace(1,length(gene_pool),n_bins+1));
+bin_edges=round(linspace(1,length(gene_pool),options.n_bins+1));
 [genebin_id, bin_edges]=discretize(1:numel(gene_pool),bin_edges);
 
 %compute query gene scores
@@ -36,8 +32,9 @@ qbins=unique(genebin_id(qixs));
 bg_ix=[];
 for i=1:length(qbins)
     this_bin=find(genebin_id==qbins(i));
-    rand_ix=this_bin(randperm(length(this_bin),ctrl_size));          %could use randomsample instead.
-%     rand_ix=this_bin(randi([1,length(this_bin)],1,ctrl_size));
+    rand_ix=randsample(this_bin,options.ctrl_size); 
+%     rand_ix=this_bin(randperm(length(this_bin),options.ctrl_size));          %could use randsample instead.
+%     rand_ix=this_bin(randi([1,length(this_bin)],1,options.ctrl_size));
     bg_ix=[bg_ix;ixs(rand_ix)]; %gene IDs from the this bin
 end
 bg_ix=setdiff(bg_ix,qix); %don't include query genes
