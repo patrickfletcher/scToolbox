9563aff87066f8eb78c967a0cb39b5c9e6bec4fd
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2021-10-26 09:01:52 -0400

    gprof_orth standardize missing

--- gprof_orth.m
+++ gprof_orth.m
@@ -14,3 +14,7 @@ orthgenes=unique(orthTab(:,{'ortholog_ensg','name'}),'rows');
 orthgenes=renamevars(orthgenes,'ortholog_ensg','id');
 orthgenes=sortrows(orthgenes,'name');
 orthgenes(orthgenes.id=="N/A",:)=[];
+
+dclass=cellfun(@(x)class(x),orthTab.description,'UniformOutput',false);
+orthTab.description(ismember(dclass,'py.NoneType'))={''};
+orthTab=standardizeMissing(orthTab,{'N/A','nan'});
