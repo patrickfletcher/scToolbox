95c939072f15d557cbacd767f3b0da69e1d545d1
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2021-12-03 05:36:45 -0500

    Update dotplot.m

--- dotplot.m
+++ dotplot.m
@@ -15,6 +15,7 @@ arguments
     options.gap=0.1
     options.area_range=[1,144]
     options.min_prct = 1
+    options.all_expr_prct=100
     options.prct_leg=3
     options.prct_leg_height=0.05
     options.cb_prct_gap=0.01
@@ -61,14 +62,19 @@ EXPR=EXPR(:,ctix);
 PRCT=PRCT(:,ctix);
 
 lowpct=all(PRCT<options.min_prct,2);
-EXPR(lowpct,:)=[];
-PRCT(lowpct,:)=[];
-if options.verbose && nnz(lowpct)>0
+all_expr=all(PRCT>options.all_expr_prct,2);
+
+discard=lowpct|all_expr;
+EXPR(discard,:)=[];
+PRCT(discard,:)=[];
+if options.verbose && nnz(discard)>0
     disp("Genes expressed in less than min_prct for all cell groups:")
     disp(G(lowpct)')
+    disp("Genes expressed in more than all_expr_prct for all cell groups:")
+    disp(G(all_expr)')
 end
-G(lowpct)=[];
-Ggrp(lowpct)=[];
+G(discard)=[];
+Ggrp(discard)=[];
 
 %other filters? ctname-max/min pct list
 
