1cb709d1989ce7cb2f6d7a6e6cf2da5f1c1d2c49
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-04-06 23:20:04 -0400

    Update leiden.py

--- leiden.py
+++ leiden.py
@@ -22,8 +22,8 @@ from sklearn.neighbors import kneighbors_graph
 def get_adjacency(coords, k_NN=None):
     if k_NN is None:
         k_NN = np.sqrt(coords.shape[0])
-    
-    k_NN = int(k_NN) 
+
+    k_NN = int(k_NN)
 
     # sklearn knn distance graph... would be better to do SNN?
     kw_knn_args = {'n_neighbors': k_NN, 'mode': 'distance', 'n_jobs': -1}
@@ -38,54 +38,148 @@ def get_adjacency(coords, k_NN=None):
 
 # def run(adjacency, resolution, k_NN=None, rng_seed=42, n_iterations=4):
 #     N = adjacency.shape[0]
-#     # build igraph object    
+#     # build igraph object
 #     sources, targets = adjacency.nonzero()
 #     weights = adjacency[sources, targets]
 #     if isinstance(weights, np.matrix):
 #         weights = weights.A1
 
-def run(N, sources, targets, weights, resolution, k_NN=None, rng_seed=42, n_iterations=4):
-    
+
+def find_partition(
+    N,
+    sources,
+    targets,
+    partition_type,
+    weights=None,
+    node_sizes=None,
+    initial_membership=None,
+    resolution=1,
+    n_iterations=-1,
+    max_comm_size=0,
+    rng_seed=None
+):
+
+    # graph from adjacency matrix (passed in as sparse representation: N, sources, targets, weights)
     g = ig.Graph(directed=True)
     g.add_vertices(N)  # this adds adjacency.shape[0] vertices
     g.add_edges(list(zip(sources, targets)))
-    try:
-        g.es['weight'] = weights
-    except:
-        print('tried g.es[''weight''] = weights but failed')
 
-    # now run leiden
-    kw_la_args = {
-        'n_iterations': n_iterations,
-        'seed': rng_seed
-    }
-    kw_la_args['weights'] = weights
-    # kw_la_args['partition_type'] = la.CPMVertexPartition
-    # kw_la_args['resolution_parameter'] = resolution
-    kw_la_args['partition_type'] = la.RBConfigurationVertexPartition
-    kw_la_args['resolution_parameter'] = resolution
+    # handle the possible partitions and their allowed args
+    partition_kwargs = {}
+    if partition_type is None:
+        partition_type = la.RBConfigurationVertexPartition
+
+    elif partition_type == "Modularity":
+        partition_type = la.ModularityVertexPartition
+        resolution = None
+        node_sizes = None
+
+    elif partition_type == "RBC":
+        partition_type = la.RBConfigurationVertexPartition
+        node_sizes = None
+
+    elif partition_type == "RBERVertexPartition":
+        partition_type = la.RBERVertexPartition
+
+    elif partition_type == "CPM":
+        partition_type = la.CPMVertexPartition
+
+    elif partition_type == "Significance":
+        partition_type = la.SignificanceVertexPartition
+        resolution = None
+        weights = None
+
+    elif partition_type == "Surprise":
+        partition_type = la.SignificanceVertexPartition
+        resolution = None
+
+    else:
+        print(
+            f"bad partition_type: {partition_type}. Using RBConfigurationVertexPartition")
+        partition_type = la.RBConfigurationVertexPartition
+
+    if weights is not None:
+        g.es['weight'] = weights
+
+    if resolution is not None:
+        partition_kwargs['resolution_parameter'] = resolution
+
+    if node_sizes is not None:
+        partition_kwargs['node_sizes'] = node_sizes
 
     start_time = time.time()
-    partition = la.find_partition(g, **kw_la_args)
-    labels = np.array(partition.membership)
-    nclust = np.unique(labels).max()+1
-    print(f"leidenalg took {time.time()-start_time}s")
-    print(f"number of clusters = {nclust}")
 
-    return labels
+    # find a partition
+    ## leidenalg.find_partition(graph, partition_type, initial_membership=None, weights=None, n_iterations=2, max_comm_size=0, seed=None, **kwargs)
+    partition = la.find_partition(g, partition_type, initial_membership=initial_membership, weights=weights,
+                                  n_iterations=n_iterations, max_comm_size=max_comm_size, seed=rng_seed, **partition_kwargs)
+
+    groups = np.array(partition.membership)
+    print(f"leidenalg took {time.time()-start_time}s")
+    print(f"number of clusters = {len(np.unique(groups))}")
+
+    return partition
     # write the labels to CSV
 
 
-# plot
-# fig = plt.figure(figsize=(4, 4), dpi=144)
-# ax = fig.add_subplot(111)
-# palette = sns.color_palette('deep', np.unique(labels).max() + 1)
-# colors = [palette[x] for x in labels]
-# plt.scatter(coords[:, 0], coords[:, 1], c=colors)
-# plt.title(f'{nclust} clusters found by Leiden', fontsize=8)
-# frame = plt.gca()
-# frame.axes.get_xaxis().set_visible(False)
-# frame.axes.get_yaxis().set_visible(False)
-# plt.tight_layout()
-# # plt.savefig(figfileroot + 'umap_modularity.png')
-# plt.show()
+def resolution_profile(
+    N,
+    sources,
+    targets,
+    partition_type,
+    resolution_range,
+    weights=None,
+    min_diff_bisect_value=1,
+    min_diff_resolution=0.001,
+    linear_bisection=False,
+    number_iterations=1,
+    rng_seed=None
+):
+
+    # graph from adjacency matrix (passed in as sparse representation: N, sources, targets, weights)
+    g = ig.Graph(directed=True)
+    g.add_vertices(N)  # this adds adjacency.shape[0] vertices
+    g.add_edges(list(zip(sources, targets)))
+
+    # handle the possible partitions and their allowed args
+    partition_kwargs = {}
+    if partition_type is None:
+        partition_type = la.RBConfigurationVertexPartition
+
+    elif partition_type == "RBC":
+        partition_type = la.RBConfigurationVertexPartition
+        node_sizes = None
+
+    elif partition_type == "RBERVertexPartition":
+        partition_type = la.RBERVertexPartition
+
+    elif partition_type == "CPM":
+        partition_type = la.CPMVertexPartition
+
+    else:
+        print(
+            f"bad partition_type: {partition_type}. Using RBConfigurationVertexPartition")
+        partition_type = la.RBConfigurationVertexPartition
+
+    if weights is not None:
+        g.es['weight'] = weights
+
+    if node_sizes is not None:
+        partition_kwargs['node_sizes'] = node_sizes
+
+    start_time = time.time()
+
+    optimiser = la.Optimiser()
+
+    if rng_seed is not None:
+        optimiser.set_rng_seed(rng_seed)
+
+    # resolution_profile(graph, partition_type, resolution_range, weights=None, bisect_func=<function Optimiser.<lambda>>,
+    #                   min_diff_bisect_value=1, min_diff_resolution=0.001, linear_bisection=False, number_iterations=1, **kwargs)
+    profile = optimiser.resolution_profile(g, partition_type, resolution_range, weights=weights, min_diff_bisect_value=min_diff_bisect_value, 
+        min_diff_resolution=min_diff_resolution, linear_bisection=linear_bisection, number_iterations=number_iterations, **partition_kwargs)
+
+    print(f"leidenalg took {time.time()-start_time}s")
+    # print(f"number of clusters = {len(np.unique(groups))}")
+
+    return profile
