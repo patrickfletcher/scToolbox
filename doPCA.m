30549647bc98f977a4f76ae64f09158f0df1b2f4
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-04-06 23:18:58 -0400

    Update doPCA.m
    arguments block

--- doPCA.m
+++ doPCA.m
@@ -1,23 +1,41 @@
-function result = doPCA(tcounts, genes, params, hvgix, figID, pcix)
+function result = doPCA(tcounts, genes, params, options)
+arguments
+    tcounts
+    genes
+    params.hvgix=[]
+    params.scale_method='zscore'
+    params.maxScaled=10
+    params.npc=0
+    params.permute_reps=100
+    options.verbose=true
+    options.figID=[]
+    options.pcix=[]
+end
 
-verbose = true;
-result.params = params;
+result = params;
 
-if exist('hvgix','var')&&~isempty(hvgix)
-    X=tcounts(hvgix,:);
+if ~isempty(params.hvgix)
+    X=tcounts(params.hvgix,:);
 else
     X=tcounts;
 end
 
 %is rescaling necessary? covariance vs correlation? logtransformed counts are roughly on same scale, in [0,3]
+
+%TODO: create function handle
+scale_args = [];
+scale_fun = @(x) x;
 switch params.scale_method
     case 'zscore'
         % Seurat scaling
-        X=(X-mean(X,2))./std(X,[],2); %zscore gene distributions
+%         X=(X-mu)./sig; %zscore gene distributions
         
+        scale_args.xmean=mean(X,2);
+        scale_args.xstd=std(X,[],2);
+        scale_fun = @(x, args) (x-args.xmean)./args.xstd;
+
         %notes
         % - largest Zscores occur for small-mean genes.
-        % - 
                 
 %         if doPlot
 %             figure(10);clf
@@ -28,44 +46,53 @@ switch params.scale_method
 %             xlabel('# cells Z-score > max')
 %             ylabel('# of genes')
 %         end
-        X(X>params.maxScaled)=params.maxScaled;
-        X(X<-params.maxScaled)=-params.maxScaled;  %
         
     case 'meanmad'
         %use median/mad as more robust measures of center and scale...
         %however, smaller scale values ==> larger standardized. Those will
         %hold more weigh, so clipping even more important...
-        MEAN=mean(X,2);
+        MU=mean(X,2);
         MAD=mad(X,0,2); 
-        X=(X-MEAN)./MAD;
+        MAD(MU==0 & MAD==0)=1; %not sure if need to have MU=0...???
         
-        X(X>params.maxScaled)=params.maxScaled;
-        X(X<-params.maxScaled)=-params.maxScaled;
+        scale_args.xmean=MU;
+        scale_args.xmad=MAD;
+        scale_fun = @(x, args) (x-args.xmean)./args.xmad;
+        
         
     case 'medmad'
         %use median/mad as more robust measures of center and scale
         MED=median(X,2);
         MAD=mad(X,1,2); 
         MAD(MED==0 & MAD==0)=1; %zero/zero occurs. Setting MAD=1 => X unchanged: X=(X-0)/1
-        X=(X-MED)./MAD;
+        scale_args.xmed=MED;
+        scale_args.xmad=MAD;
+        scale_fun = @(x, args) (x-args.xmed)./args.xmad;
         
     case 'unit'
         %unitize
-        X=(X-min(X,[],2))./(max(X,[],2)-min(X,[],2));
-        
-    case 'center_unit'
-        %unitize, then subtract mean
-        X=(X-mean(X,2))./(max(X,[],2)-min(X,[],2));
+        scale_args.xmin=min(X,[],2);
+        scale_args.xmax=min(X,[],2);
 
-    case 'center'
-        X=(X-mean(X,2));
+        scale_fun = @(x,args) (x-args.xmin)./(args.xmax-args.xmin);
         
-    case 'none'
+    case 'center'
+        scale_args = mean(x,2);
+        scale_fun = @(x, mu) x-mu;
+        
 end
+
+result.scale_args=scale_args;
+result.scale_fun=scale_fun;
+
+X = scale_fun(X, scale_args);
+
+X(X>params.maxScaled)=params.maxScaled;
+X(X<-params.maxScaled)=-params.maxScaled;
 
 if params.npc<1
     % Find number of significant PCs
-    if verbose, disp('Performing PCA permutation test...'), end
+    if options.verbose, disp('Performing PCA permutation test...'), end
     [npc,p,e,d]=findSignificantPCs(X',params.permute_reps,0.01);
     result.npc=npc;
     result.p=p;
@@ -74,37 +101,50 @@ if params.npc<1
 else
     result.npc=params.npc;
 end
-if verbose, disp(['Number of PCs: ',num2str(result.npc)]), end
+if options.verbose, disp(['Number of PCs: ',num2str(result.npc)]), end
 
 % do the pca
-if verbose, disp('Computing PCA...'), end
+if options.verbose, disp('Computing PCA...'), end
 [coeff,score]=fast_pca(X',result.npc);
-
-pc_gene_name(size(X,1),result.npc)="";
-% pc_gene_id(size(X,1),result.npc)="";
-pc_gene_ix(size(X,1),result.npc)=0;
-for i=1:length(coeff(1,:))
-    [~,ix]=sort(coeff(:,i),'descend');
-    if exist('hvgix','var')&&~isempty(hvgix)
-        pc_gene_name(:,i)=genes.name(hvgix(ix));
-%         pc_gene_id(:,i)=genes.id(hvgix(ix));
-        pc_gene_ix(:,i)=hvgix(ix);
-    else
-        pc_gene_name(:,i)=genes.name(ix);
-%         pc_gene_id(:,i)=genes.id(ix);
-        pc_gene_ix(:,i)=ix;
-    end
-end
-
+ 
 result.coeff=coeff;
 result.coords=score;
+
+% pc_gene_name(size(X,1),result.npc)="";
+% % pc_gene_id(size(X,1),result.npc)="";
+% pc_gene_ix(size(X,1),result.npc)=0;
+% for i=1:length(coeff(1,:))
+%     [~,ix]=sort(coeff(:,i),'descend');
+%     if ~isempty(params.hvgix)
+%         pc_gene_name(:,i)=genes.name(params.hvgix(ix));
+% %         pc_gene_id(:,i)=genes.id(params.hvgix(ix));
+%         pc_gene_ix(:,i)=hvgix(ix);
+%     else
+%         pc_gene_name(:,i)=genes.name(ix);
+% %         pc_gene_id(:,i)=genes.id(ix);
+%         pc_gene_ix(:,i)=ix;
+%     end
+% end
 % result.pc_gene_name=pc_gene_name;
 % result.pc_gene_id=pc_gene_id;
 % result.pc_gene_ix=pc_gene_ix;
         
-if exist('figID','var')
+if ~isempty(options.figID)
+
+    pc_gene_name(size(X,1),result.npc)="";
+    pc_gene_ix(size(X,1),result.npc)=0;
+    for i=1:length(coeff(1,:))
+        [~,ix]=sort(coeff(:,i),'descend');
+        if ~isempty(params.hvgix)
+            pc_gene_name(:,i)=genes.name(params.hvgix(ix));
+            pc_gene_ix(:,i)=params.hvgix(ix);
+        else
+            pc_gene_name(:,i)=genes.name(ix);
+            pc_gene_ix(:,i)=ix;
+        end
+    end
     
-    if ~exist('pcix','var') || isempty(pcix)
+    if isempty(options.pcix)
         pcix=[1,2];
     end
     if numel(pcix)~=2
@@ -126,15 +166,13 @@ if exist('figID','var')
     
     genelist = [pc_gene_name(1,1:2),pc_gene_name(end,1:2)];
     gix=[pc_gene_ix(1,1:2),pc_gene_ix(end,1:2)];
-    colors=tcounts(gix,:);
-    figure(figID);clf
-    [ax,hs]=plotScatter(S,'value',genelist,colors,figID);
-%     for i=1:length(hs)
-%         hs(i).SizeData=5;
-%     end
-    for i=1:length(ax)
-        axis(ax(i),'equal')
-        axis(ax(i),'tight')
+    vals=tcounts(gix,:);
+
+    figure(options.figID);clf
+    hsc=scatter_val(S,vals,fig=options.figID,splitnames=genelist);
+
+    for i=1:length(hsc.ax)
+        axis(hsc.ax(i),'equal','tight')
     end
     drawnow
 end
