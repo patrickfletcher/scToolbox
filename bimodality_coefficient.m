975c32933174321555da5c592f4e88f7d63238d2
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-04-06 23:18:47 -0400

    Update bimodality_coefficient.m

--- bimodality_coefficient.m
+++ bimodality_coefficient.m
@@ -1,10 +1,10 @@
-function BC = bimodality_coefficient(X)
+function BC = bimodality_coefficient(X, dim)
 
-N = size(X,2);
+N = size(X,dim);
 
 denom_factor = 3*(N-1)^2/((N-2)*(N-3));
 
-skew = skewness(X, 0, 2);
-kurt = kurtosis(X, 0, 2) - 3;
+skew = skewness(X, 0, dim);
+kurt = kurtosis(X, 0, dim) - 3;
 
 BC = (skew.^2 + 1)./(kurt + denom_factor);
