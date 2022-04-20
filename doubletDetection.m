065acf547a58c9ce593b98ea8be5b0d598837c51
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-04-19 20:52:37 -0400

    Update doubletDetection.m

--- doubletDetection.m
+++ doubletDetection.m
@@ -1,279 +1,329 @@
-function result=doubletDetection(rawcounts, normcounts, logcounts, pca_score, pca_loadings, params)
+function result=doubletDetection(rawcounts, normcounts, logcounts, optin, params, hvgpars, pcapars,umappars, options)
 arguments
     rawcounts
     normcounts
     logcounts
-    pca_score
-    pca_loadings
+    
+    %optional input data
+    optin.hvg=[] 
+    optin.pca=[]
+    optin.genes=[]
+    optin.clusterID=[]
+
+    params.pK=[0.0005,0.001,0.003,0.005, 0.01, 0.03, 0.1]
+    params.pN=0.15:0.05:0.3
     params.rngSeed=42
-    params.libsize_method='mean'
-    params.use_true_loadings=true
+    params.pExp=0.075 %how many doublets to remove - reduce to account for homotypic
+
+    params.do_new_pca=1
+    params.nn_metric='correlation'
+
+    params.do_new_hvg=0
+
+    params.select_parent_method='random' %implement cluster based
+    params.libsize_method='add'
+    params.libsize_factor=1 %if using libsize_method='foldmax'
+
+    params.call_method="topk"
     params.do_p_correction=true
-    params.nReps=1
-    params.p_thr=0.05
-    params.n_neighbors=30
-    params.votemethod='any'
-    params.doplot=false
-    params.hvg=[]
-    params.pca=[]
-    params.umap=[]
+    params.pthr=0.05
+
+    params.n_reps=1
+    params.vote_thr=0 %call doublets present in this fraction of reps
+
+    hvgpars.nHVG=5000
+
+    pcapars.npc=50
+    pcapars.verbose=false
+    
+    %umap metric and knn will be slaved to params.nn_metric
+    umappars.n_neighbors = 30
+    umappars.min_dist = 0.3
+    umappars.n_epochs = 0
+    umappars.n_components=2
+
+    options.figID=[]
 end
 % inspired by Shor's Doublet detection (python3):
 % https://github.com/JonathanShor/DoubletDetection
+% also - scrublet, doubletFinder, scDblFinder... knn-methods.
 
-%assume PCA loadings + scores of true cells are given. Project synthetic
-%doublets into this space.
+%TODO: two use cases: 1) param sweep, 2) single run (with optional plotting). split out core work function?
+%TODO: option to reuse sweep results but call different expected #
 
-%my mod: use KNN neighborhoods instead of graph-clustering.
-% two main pars: K and Boostrate.
-% K - size of kNN nhood. small affects
+%TODO: implement do_new_hvg
 
-% several other small mods too
-% slow part: resampling parents
-
-%TODO:
-% - scanning k for kNN should be much faster than processing different
-% boostrates. Store results in a way that allows easy updates?
-
-%TODO:
-% - Project synthetic cells onto existing PCs of parent's gene space!!!!!
-% - is FDR really needed?  Also, the value of synthCount for cutoff should be fixed given n_neighbors, nSynth, nCells.
-% - options for newLibSize mode (max now, also possible: mean)
-% - replace parents true/false (true now)
+%TODO: interface - linked options (hvgix vs normcounts)
 
 %TODO: -use clustering to select parents from different clusters (homotypic
 %doublets don't help). While loop: discard homotypic, continue until nSynth
 
 rng(params.rngSeed,'simdTwister') %for speedup?
 
-nCells=size(rawcounts,2);
+[nGenes,nCells]=size(rawcounts);
+nExp=round(params.pExp*nCells);
+
+PK=params.pK;
+PN=params.pN;
 
 %initialize results
-result.params = params;
+result = params;
 
-%early exit option: nReps=0 means nothign to do.
-if params.nReps==0
-    result.doublets=false(1,nCells);
-    result.rep_counts=0;
-    return
+%get the HVGs from raw data only
+hvgargs=namedargs2cell(hvgpars);
+hvg=optin.hvg;
+if isempty(hvg) || ~isfield(hvg,'ix')
+    hvg=findVariableGenes(normcounts, optin.genes, hvgargs{:}); 
 end
-
-% augment the population with synthetic doublets
-% each doublet created from two randomly selected parents => correct
-% proportions sum counts - downsample to new lib size.
-nSynth=floor(params.boostrate*nCells);
-
-% hvg=[];
-% if params.use_true_loadings
-%     %get the HVGs from raw data only
-%     hvg=findVariableGenes(normcounts, genes, params.hvg);
-%     hvgix=hvg.ix;
-%   
-%     %logcounts can be subset now: constant
-%     logcounts=logcounts(hvgix,:); 
-%     logcounts_aug=logcounts;
-%     nGenes=length(hvgix);
-%     logcounts_aug(nGenes,nCells+nSynth)=0; %expand logcounts for synth cells
-%     
-%     %get the true data PCA space (coeffs for projection of new data). Only
-%     %possible if use_true_cell_HVG=true
-%     [coeff,pca_score, mu, sig]=fast_pca(logcounts', params.pca.npc, params.pca.maxScaled);
-%     pca_score(nCells+nSynth,params.pca.npc)=0; 
+% if ~params.do_new_hvg
+% nGenes=length(hvg.ix);
+% logcounts=logcounts(hvg.ix,:);
 % end
 
-nGenes=length(hvgix);
+%get the true data PCA space (coeffs for projection of new data)
+pca=optin.pca;
+if isempty(pca)
+    pcaargs=namedargs2cell(pcapars);
+    pca = doPCA(logcounts(hvg.ix,:), optin.genes, pcaargs{:});
+end
+pca_score = pca.coords;
 
-logcounts=logcounts(hvgix,:); 
-logcounts_aug=logcounts;
-logcounts_aug(nGenes,nCells+nSynth)=0; %expand logcounts for synth cells
-pca_score(nCells+nSynth,params.pca.npc)=0; 
+% %expand logcounts for synth cells
+% % nSynth=floor(params.boost_rate*nCells);
+% nSynth=floor(nCells/(1-params.boost_rate) - nCells); %doubletFinder method
+% logcounts_aug=logcounts;
+% logcounts_aug(nGenes,nCells+nSynth)=0; 
+% pca_score(nCells+nSynth, pca.npc)=0; 
 
-tick=round(params.nReps/10); %progress meter
-t0=tic;
-rep_score=zeros(params.nReps,nCells);
-rep_doublet=false(params.nReps,nCells);
-rep_p=ones(params.nReps,nCells);
-rep_adj_p=ones(params.nReps,nCells);
-for it=1:params.nReps
-    
-    rawcounts_synth=createSyntheticDoublets(rawcounts,nSynth,params);
+%interface: two cases. 1) param sweep, 2) final run supporting plot
+% - put the main work in functions (loop body)
+% - looping over one PN and one PK naturally works. use "squeeze" to
+% flatten after if needed.
 
-    %normalize synthetic doublets across all genes (not just hvgs)
-    normcounts_synth=normalizeCounts(rawcounts_synth);
-    
-    synth_lcounts=log10(normcounts_synth(hvgix,:)+1);
-    logcounts_aug(:,nCells+1:end)=synth_lcounts;
-    
-    X=synth_lcounts';
-    X=(X-mu)./sig;
-    X(X>params.pca.maxScaled)=params.pca.maxScaled;
-    X(X<-params.pca.maxScaled)=-params.pca.maxScaled;
-    pca_score(nCells+1:end,:)=X*coeff;
-    
-    this_rep_doublets=false(1,nCells);
-            
-    %get the kNN of each cell, compute fraction of synth doublets in
-    %this neighborhood.
-    %synthCount=number of true cell's neighbors that are synth
-    
-    nnix=knnsearch(pca_score,pca_score,'K',params.n_neighbors+1); nnix(:,1)=[];
-    synthCount=sum(nnix(1:nCells,:)>nCells,2); %all synthetic cells have index >nCells
-    p=hygecdf(synthCount(:),nCells+nSynth,nSynth,params.n_neighbors,'upper');
-    
-    [~,~, ~, adj_p]=fdr_bh(p,params.p_thr);
-    
-    if params.do_p_correction
-        this_rep_doublets=adj_p<params.p_thr;
-    else
-        this_rep_doublets=p<params.p_thr;    
+% different data structure is needed for param sweep...
+% - K by BR grid of synthScores (3D array)
+% - for each k, get BC
+% - BCmvn=mean(BC)/var(BC), mean&var across BR.
+% - find peak BCmvn (save this curve, for verfication)
+% -- also save the hygecdf vals...
+doublet_scores=zeros(length(PK), nCells, length(PN));
+p_hyge=ones(length(PK), nCells, length(PN));
+doublets = false(length(PK), nCells, length(PN));
+% doublets_hyge = false(length(PK), nCells, length(PN));
+BC=zeros(length(PN),length(PK));
+
+% tick=round(params.nReps/10); %progress meter
+% t0=tic;
+% rep_score=zeros(params.nReps,nCells);
+% rep_doublet=false(params.nReps,nCells);
+% rep_p=ones(params.nReps,nCells);
+% rep_adj_p=ones(params.nReps,nCells);
+% for it=1:params.nReps
+for i=1:length(PN)
+    disp("pN = " + string(PN(i)))
+    tic
+
+    %expand logcounts for synth cells
+    nSynth=floor(PN(i)*nCells);
+%     nSynth=floor(nCells/(1-PN(i)) - nCells); %doubletFinder method
+    if params.do_new_hvg
+        normcounts_aug=normcounts;
+        normcounts_aug(nGenes,nCells+nSynth)=0; 
     end
+    logcounts_aug=logcounts;
+    logcounts_aug(nGenes,nCells+nSynth)=0; 
+    pca_score = pca.coords;
+    pca_score(nCells+nSynth, pca.npc)=0; 
+
+    for j=1:length(PK)
+
+        rawcounts_synth=createSyntheticDoublets(rawcounts,nSynth,params, optin.clusterID);
     
-%     disp("repdoubs: "+num2str(nnz(this_rep_doublets)))
-    rep_score(it,:)=synthCount;
-    rep_doublet(it,:)=this_rep_doublets; 
-    rep_p(it,:)=p;
-    rep_adj_p(it,:)=adj_p;  %store p vals so re-thresholding can be done
-    
-    if tick>0 && mod(it,tick)==0
-        fprintf('.%d(%0.1fs)',it,toc)
-    elseif tick==0
+        %normalize synthetic doublets across all genes (not just hvgs)
+        synth_ncounts=normalizeCounts(rawcounts_synth);
+
+        if params.do_new_hvg
+            normcounts_aug(:,nCells+1:end)=synth_ncounts;
+            hvg=findVariableGenes(normcounts_aug, optin.genes, hvgargs{:}); 
+        end
+
+        synth_lcounts=log10(synth_ncounts(hvg.ix,:)+1);
+        logcounts_aug(hvg.ix,nCells+1:end)=synth_lcounts;
+        
+        if params.do_new_pca
+            pcaargs=namedargs2cell(pcapars);
+            newpca = doPCA(logcounts_aug(hvg.ix,:), optin.genes, pcaargs{:});
+            pca_score = newpca.coords;
+        else
+            %project synthetic doublets into PC space
+            X=synth_lcounts;
+            X=pca.scale_fun(X, pca.scale_args);
+            X(X>pca.maxScaled)=pca.maxScaled;
+            X(X<-pca.maxScaled)=-pca.maxScaled;
+            pca_score(nCells+1:end,:)=X'*pca.coeff;
+        end
+                
+        %get the kNN of each cell, compute fraction of synth doublets in
+        %this neighborhood.
+        K = round(PK(j)*nCells);
+        [nnix, nndists]=knnsearch(pca_score, pca_score,K=K+1,Distance=params.nn_metric); 
+        nnix(:,1)=[]; nndists(:,1)=[]; 
+
+        %synthCount=number of true cell's neighbors that are synth
+        synthCount=sum(nnix(1:nCells,:)>nCells,2); %all synthetic cells have index >nCells
+        synthScore=synthCount/K;
+        p=hygecdf(synthCount(:),nCells+nSynth, nSynth, K,'upper');
+%         [~,~, ~, adj_p]=fdr_bh(p, params.p_thr);
+
+        doublet_scores(j,:,i)=synthScore;
+        p_hyge(j,:,i)=p;
+
+        %call doublets in two ways: hyge and top n (expected)
+        switch params.call_method
+            case "topk"
+                [~, top_ix]=maxk(synthScore,nExp);
+                doublets(j,top_ix,i)=true;
+            case "pval"
+                if params.do_p_correction
+                    [~,~,~,p]=fdr_bh(p);
+                end
+                doublets(j,p<params.pthr,i)=true;
+        end
+        
         fprintf('.')
     end
+    BC(i,:)=bimodality_coefficient(doublet_scores(:,:,i),2); %BC computed across cells
+
+    fprintf('(%0.1fs)',toc)
+    fprintf('\n')
 end
-fprintf('\n')
 
-rep_counts=sum(rep_doublet,2);
+meanBC=mean(BC,1);
+varBC=var(BC,0,1);
+if length(PN)==1
+    varBC=ones(size(meanBC));
+end
+BCmvn = meanBC./varBC;
+[topBCmvn, topBCKix]=max(BCmvn);
+%use the top K selection to choose the top N
+[topBC, topBCNix]=max(BC(:,topBCKix));
 
-%voting methods for calling doublets accounting for all rep results
+usedoublets=squeeze(doublets(topBCKix,:,topBCNix));
+usescores=squeeze(doublet_scores(topBCKix,:,topBCNix));
+usep=squeeze(p_hyge(topBCKix,:,topBCNix));
+
+result.bestPK=PK(topBCKix);
+result.bestPN=PN(topBCNix);
+result.doublets=usedoublets;
+result.doublet_scores=usescores;
+result.p_hyge=usep;
+
+%for detailed investigation:
+result.all_scores=doublet_scores;
+result.all_p_hyge=p_hyge;
+result.all_doublets=doublets;
+result.BC=BC;
+result.BCmvn=BCmvn;
+result.topBCmvn=topBCmvn;
+result.topBC=topBC;
+
+% rep_counts=sum(rep_doublet,2);
+% 
+% %voting methods for calling doublets accounting for all rep results
 % if params.vote_thr==0
 %     doublet=sum(rep_doublet,1)>params.vote_thr*params.nReps;
 % else
 %     doublet=sum(rep_doublet,1)>=params.vote_thr*params.nReps;
 % end
 
-switch params.votemethod
-    case 'any'
-        % returns all doublets encountered in any rep
-        doublet=any(rep_doublet,1);
-    case 'all'
-        % returns only consensus doublets that were called in all reps
-        doublet=all(rep_doublet,1);
-    case 'majority'
-        % returns doublets that were called in at least half of reps
-        doublet=sum(rep_doublet,1)>=0.5*params.nReps;
-    case 'p10'
-        % returns doublets that were called in more than 10% of reps
-        doublet=sum(rep_doublet,1)>0.1*params.nReps;
-    case 'p90'
-        % returns doublets that were called in more than 90% of reps
-        doublet=sum(rep_doublet,1)>0.9*params.nReps;
-    otherwise
-        warning('invalid option for aggregating doublets found across repetitions')
-        doublet=all(rep_doublet,1);
-end
+% result.doublet=doublet;
+% result.rep_score=rep_score;
+% result.rep_p=rep_p;
+% result.rep_adj_p=rep_adj_p;
+% result.rep_doublet=rep_doublet;
+% result.rep_counts=rep_counts;
+% result.nSynth=nSynth;
 
 %do a umap for the final rep for demo purposes
 umap=[];
 group=[];
-groupcols=[1,1,1; 0,0.8,0; 0,0,0];
-if params.doplot
+groupcols=[0.8,0.8,0.8; 0,0.8,0; 0,0,0];
+if ~isempty(options.figID)
+
     group=zeros(1,size(pca_score,1));
-    group(1:nCells)=doublet;
-%     group(1:nCells)=this_rep_doublets; 
+    group(1:nCells)=usedoublets;
     group(nCells+1:end)=2; %synth
     group=categorical(group,[0,2,1],{'tc','sd','td'});
 
-    umap=doUMAP(pca_score, params.umap);
-    fh=figure();
-    plotScatter(umap.coords, 'group', group, groupcols, fh);
+    %could just do a new sim at the best K/N combo...
+
+    knn.indices=nnix; knn.dists=nndists;
+    umappars.metric = params.nn_metric;
+    umapargs=namedargs2cell(umappars);
+    umap=doUMAP(pca_score, knn, umapargs{:});
+
+    figure(options.figID); clf
+    scatter_grp(umap.coords, group, gcols=groupcols, fig=options.figID);
+    axis tight equal
     drawnow
-end
 
-result.doublet=doublet;
-result.rep_score=rep_score;
-result.rep_p=rep_p;
-result.rep_adj_p=rep_adj_p;
-result.rep_doublet=rep_doublet;
-result.rep_counts=rep_counts;
-result.nSynth=nSynth;
-result.hvg=hvg;
-result.umap=umap;
-result.group=group;
-result.groupcols=groupcols;
+    result.umap=umap;
+    result.group=group;
+    result.groupcols=groupcols;
+end
 
 end
 
-function rawcounts_synth=createSyntheticDoublets(rawcounts,nSynth,params)
+function rawcounts_synth=createSyntheticDoublets(rawcounts, nSynth, params, clustID)
 
 [nGenes,nCells]=size(rawcounts);
 rawcounts_synth=zeros(nGenes,nSynth);
 for j=1:nSynth
-    parents=rawcounts(:,randi(nCells,1,2)); %sample parents with replacement
+    switch params.select_parent_method
+        case 'random'
+            parents=rawcounts(:,randi(nCells,1,2)); %sample parents with replacement
+        case 'cluster'
+            %ensure parents are from different clusters (force heterotypic parents)
+            %select based on cluster proportions
+%             tbl=tabulate(clusterID);
+%             prop_c=tbl.Count./length(clusterID);
+            pix1=randi(nCells); %first parent select randomly
+            p1clust=clustID(pix1);
+            remaining=find(clustID~=p1clust);
+            pix2=remaining(randi(length(remaining)));
+            parents=rawcounts(:,[pix1,pix2]); 
+    end
     
     parentLibSize=sum(parents,1); %options: max/min/mean/etc? 
     %options for new libsize: max/mean/other? assume doublets have more reads?
+    doSubSample=true;
+    newSynth=sum(parents,2);
     switch params.libsize_method
+        case 'add'
+            doSubSample=false;
+        case 'simplemean'
+            newSynth=newSynth/2;
+            doSubSample=false;
+        case 'simplemax'
+            newSynth=max(parents,[],2);
+            doSubSample=false;
         case 'mean'
             synth_libsize=round(mean(parentLibSize));
         case 'min'
             synth_libsize=min(parentLibSize); 
         case 'max'
             synth_libsize=max(parentLibSize);  
-        case 'oversample'
+        case 'foldmax'
             %assume doublets have more reads?
             synth_libsize=round(max(parentLibSize)*params.libsize_factor);
     end
-    
-    newSynth=sum(parents,2);
-    edges=[0;cumsum(newSynth)];
-    perm=randperm(sum(parentLibSize),synth_libsize); %sample gene counts without replacement
-    rawcounts_synth(:,j)=histcounts(perm,edges)';
+
+    if doSubSample
+        edges=[0;cumsum(newSynth)];
+        perm=randperm(sum(parentLibSize),synth_libsize); %sample gene counts without replacement
+        newSynth=histcounts(perm,edges)';
+    end
+    rawcounts_synth(:,j)=newSynth;
 end
     
 end
-
-
-
-
-% function [rawcounts_synth,parentIDs,max_parent,synth_libsize]=createSyntheticDoublets(rawcounts,nSynth)
-% 
-% [nGenes,nCells]=size(rawcounts);
-% rawcounts_synth=zeros(nGenes,nSynth);
-% parentIDs=zeros(2,nSynth);
-% max_parent=zeros(1,nSynth);
-% synth_libsize=zeros(1,nSynth);
-% for j=1:nSynth
-%     parentIDs(:,j)=randi(nCells,1,2);
-%     parents=rawcounts(:,parentIDs(:,j)); %sample parents with replacement
-%     
-%     parentLibSize=sum(parents,1); %options: max/min/mean/etc? 
-% %     synth_libsize(j)=round(mean(parentLibSize)); %assume doublets have more reads?
-% %     ix=1; %assume doublets have more reads?
-%     [synth_libsize(j),ix]=max(parentLibSize); %assume doublets have more reads? 
-%     max_parent(j)=parentIDs(ix,j);
-%     
-%     newSynth=sum(parents,2);
-%     edges=[0;cumsum(newSynth)];
-%     perm=randperm(sum(parentLibSize),synth_libsize(j)); %sample gene counts without replacement
-%     rawcounts_synth(:,j)=histcounts(perm,edges)';
-% %     perm=randperm(sum(parentLibSize));
-% %     synthDoublets(:,j)=histcounts(perm(1:newLibSize),edges)';
-% end
-
-
-    
-    %     %vectorized - different answer, diff rng stream? so far can't vectorize randperm/histcount. is there another way?
-    %     parentIDs=randi(nCells,2,nSynth);
-    %     parents=cat(3,rawcounts(:,parentIDs(1,:)),rawcounts(:,parentIDs(2,:)));
-    %     parentLibSize=squeeze(sum(parents,1))';
-    %     newLibSize=max(parentLibSize,[],1);
-    %     synthDoublets=zeros(nGenes,nSynth);
-    %     for j=1:nSynth
-    %         newSynth=sum(parents(:,j,:),3);
-    %         edges=[0;cumsum(newSynth)];
-    %         perm=randperm(sum(parentLibSize(:,j)));
-    %         synthDoublets(:,j)=histcounts(perm(1:newLibSize(j)),edges)';
-    %     end
-    
-    %     rawcounts_synth=[rawcounts(resample_genes,:),synthDoublets];
-    %     logcounts_synth=log10(normalizeCounts(rawcounts_synth)+1);
