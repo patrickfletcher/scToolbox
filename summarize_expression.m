999ff72d5fc896865fa308762dc223a7d9998427
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-04-19 20:55:13 -0400

    Create summarize_expression.m

---
+++ summarize_expression.m
@@ -0,0 +1,23 @@
+function expr_summary = summarize_expression(genes,ncounts,group,options)
+arguments
+    genes
+    ncounts
+    group = []
+    options.block = []
+    options.method = 'mean'
+    options.trim_prct = 0
+    options.doPrct = true
+    options.doPooled = false %true: Cat1, nonCat1, Cat2, ... 
+    options.only_expressing = false;
+end
+%streamlined version of expression summary table (no more thresholds)
+
+
+%remove cats first?
+if isempty(group)
+    group=ones(size(ncounts,2),1);
+end
+if ~iscategorical(group)
+    group=categorical(group);
+end
+group=removecats(group);
