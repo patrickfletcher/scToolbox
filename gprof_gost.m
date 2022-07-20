d820ab2890283c25a7696182be40ca6d6d498841
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-07-20 19:37:01 -0400

    Update gprof_gost.m

--- gprof_gost.m
+++ gprof_gost.m
@@ -2,12 +2,15 @@ function res = gprof_gost(id_list, organism, options)
 arguments
     id_list
     organism
+    options.gene_list = []
     options.Ordered = false
     options.Sources = {'GO'}
     options.no_evidences = false
     options.user_threshold = 0.05
     options.significance_threshold_method = 'g_SCS'
 end
+%TODO: consider supporting gene table as id_list? if table, use
+%genes.id/genes.name
 
 % g:Profiler query
 args=pyargs('base_url','https://biit.cs.ut.ee/gprofiler_archive3/e104_eg51_p15/');
@@ -28,8 +31,16 @@ res=gp.profile(gpargs);
 
 res=Core_py2matlab(res);
 res=[res{:}];
-res=struct2table(res);
 
-res.expectedfrac=res.term_size./res.effective_domain_size;
-res.expected=res.expectedfrac.*res.query_size;
-res.fold_enrichment=res.intersection_size./res.expected;
+%some post-processing
+if ~isempty(res) && isstruct(res)
+    res=struct2table(res);
+    res.expectedfrac=res.term_size./res.effective_domain_size;
+    res.expected=res.expectedfrac.*res.query_size;
+    res.fold_enrichment=res.intersection_size./res.expected;
+
+    if ~isempty(options.gene_list)
+        res.int_names=arrayfun(@(x)sort(options.gene_list(ismember(x{:},id_list))),res.intersections,'UniformOutput',false);
+    end
+
+end
