0b5c90574ca9776ac0d7898a44fe59f8184446c7
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-09-06 22:06:45 -0400

    Create rot.m

---
+++ rot.m
@@ -0,0 +1,13 @@
+function X = rot(X,theta)
+switch size(X,2)
+    case 2
+        R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+    case 3
+        t=theta(1); u=theta(2); v=theta(3);
+        Rx = [1 0 0; 0 cos(t) -sin(t); 0 sin(t) cos(t)];
+        Ry = [cos(u) 0 sin(u); 0 1 0; -sin(u) 0 cos(u)];
+        Rz = [cos(v) -sin(v) 0; sin(v) cos(v) 0; 0 0 1];
+        R=Rx*Ry*Rz;
+end
+
+X=X*R;
