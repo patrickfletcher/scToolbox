58213cd3bf94fbb1304f4c7efb47bfec21a36d51
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-09-06 22:06:10 -0400

    update ggalluvial

--- ggalluvial.R
+++ ggalluvial.R
@@ -18,8 +18,11 @@ library(ggalluvial)
 # fillvar <- unlist(mat$fillvar)    
 
 setwd("C:/Users/fletcherpa/Box/scRNAseq/rat/pineal/matlab")
-
 datafile <- "cleaned_pinast_age_by_clust.csv"
+
+
+# datafile='D:/tmp/tmp_ggalluvial.csv';
+
 data <-read.csv(datafile, header = T)
 
 clustnames<-as.character(sort(unique(data$Cluster)))
@@ -35,15 +38,28 @@ tab <- table(Cluster=clust, Age=age)
 
 df<-as.data.frame(tab)
 df2<-as.data.frame(proportions(tab,2))
-df$Proportion<-df2$Freq
+df$Proportion<-df2$Freq*100
 
+x<-c(rep(-3,11),rep(-1,11),rep(5,11),rep(20,11),rep(40,11))
+
+df$x=x
 # alluvial plot
 ggplot(df,
-       aes(x=Age, y=Proportion,
-           stratum = Cluster, alluvium = Cluster,
-           fill = Cluster, label = Cluster)) +
-  geom_flow(stat = "alluvium", lode.guidance = "frontback",
-            color = "darkgray") +
-  geom_stratum(alpha = .75) +
-  scale_x_discrete(expand = c(.05, .05)) + 
+       aes(x=x, 
+           y=Proportion,
+           stratum = Cluster, 
+           alluvium = Cluster,
+           fill = Cluster, 
+           label = Cluster)) +
+  geom_flow(stat = "alluvium", 
+            color = "gray32",
+            alpha = .5) +
+  geom_stratum(stat="stratum",
+               alpha = 1) +
+  scale_x_continuous(breaks=c(0,20,40),
+                     expand = c(.05, .05)) + 
   scale_fill_manual(values=cols) & theme_minimal()
+
+# lode.guidance = "frontback",
+# 
+# scale_x_discrete(expand = c(.05, .05)) + 
--- ggalluvial.m
+++ ggalluvial.m
@@ -1,12 +1,67 @@
-function ggalluvial(data,options)
+function ggalluvial(data,strat_var,alluv_var,fill_var,cols,figurefile,plotopts,figopts,options)
 arguments
     data
-    options.cols
+    strat_var
+    alluv_var
+    fill_var
+    cols
+    figurefile
+    plotopts.xvals="NULL"
+    plotopts.yvals="proportions"
+    plotopts.strat_levels="NULL"
+    plotopts.alluv_levels="NULL"
+    plotopts.fill_levels="NULL"
+    figopts.width=6
+    figopts.height=4
+    figopts.resolution=150
+    options.parfile="D:\tmp\tmp_scran_score_markers.mat"
+    options.verbose=false
+end 
+
+if options.verbose
+    disp("Running " + mfilename + "...")
 end
 
+%do the summary table here, write a smaller CSV. it is the ggplot df
 
+typeage=stratify_factors(cells.age(:),type(:));
+summary(typeage)
 
-if ~isempty(options.cols)
-    hexcols=rgb2hex(options.cols);
-    hexcols=string(hexcols);
+agecnt=countcats(cells.age);
+typeagecnt=countcats(typeage); 
+typeagecnt=reshape(typeagecnt,K,[])';
+
+datafile='D:\tmp\tmp_ggalluvial.csv';
+writetable(data,datafile)
+
+figurefile=cellstr(figurefile);
+strat_var=cellstr(strat_var);
+alluv_var=cellstr(alluv_var);
+fill_var=cellstr(fill_var);
+
+% if ~isempty(cols)
+    cols=cellstr(rgb2hex(cols));
+% end
+
+save(options.parfile,"datafile","figurefile","strat_var","alluv_var","fill_var","cols","figopts")
+
+Rpath = [FindRpath, filesep, 'Rscript.exe', '" "', '--vanilla '];
+scriptfile = mfilename('fullpath')+".R";
+command = char(strjoin([scriptfile,options.parfile]," "));
+commandline=['"', Rpath, command '"'];
+
+tic
+[status, cmdout]=system(commandline);
+
+if status~=0
+    error(cmdout)
 end
+if options.verbose
+    disp(cmdout)
+end
+disp(mfilename+": ")
+toc
+
+%load and plot the image if it's a png
+% figure
+% I=imread()
