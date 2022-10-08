8c6d24b0b8502289098d5395fb802ff8f8c844ef
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-10-08 05:45:01 -0400

    Update scatter_val.m

--- scatter_val.m
+++ scatter_val.m
@@ -256,55 +256,55 @@ hsc.scopts=scopts;
 
 end
 
-%more location options (side,low/center/high)
-function hcb=makeCB(ax,rectpos,gap,dims,placement,justify)
-    %common features of east/west or north/south
-    switch placement
-        case {'east','west'}
-            cbh=dims(1)*rectpos(4);
-            cbw=dims(2);
-            switch justify
-                case 'low'
-                    cby=rectpos(2);
-                case 'midlo'
-                    cby=rectpos(2)+(rectpos(4)-cbh)*0.25;
-                case 'mid'
-                    cby=rectpos(2)+(rectpos(4)-cbh)*0.5;
-                case 'midhi'
-                    cby=rectpos(2)+(rectpos(4)-cbh)*0.75;
-                case 'high'
-                    cby=rectpos(2)+rectpos(4)-cbh;
-            end
-        case {'north','south'}
-            cbw=dims(1)*rectpos(3);
-            cbh=dims(2);
-            switch justify
-                case 'low'
-                    cbx=rectpos(1);
-                case 'midlo'
-                    cbx=rectpos(1)+(rectpos(3)-cbw)*0.25;
-                case 'mid'
-                    cbx=rectpos(1)+(rectpos(3)-cbw)*0.5;
-                case 'midhi'
-                    cbx=rectpos(1)+(rectpos(3)-cbw)*0.75;
-                case 'high'
-                    cbx=rectpos(1)+rectpos(3)-cbw;
-            end
-    end
-    %unique features of each side: gap from axis
-    switch placement
-        case 'east'
-            cbx=rectpos(1)+rectpos(3)+gap;
-        case 'west'
-            cbx=rectpos(1)-gap-cbw;
-        case 'north'
-            cby=rectpos(2)+rectpos(4)+gap;
-        case 'south'
-            cby=rectpos(2)-gap-cbh;
-    end
-    
-
-%     [cbx,cby,cbw,cbh]
-    hcb=colorbar(ax,placement);
-    hcb.Position=[cbx,cby,cbw,cbh];
-end
+% %more location options (side,low/center/high)
+% function hcb=makeCB(ax,rectpos,gap,dims,placement,justify)
+%     %common features of east/west or north/south
+%     switch placement
+%         case {'east','west'}
+%             cbh=dims(1)*rectpos(4);
+%             cbw=dims(2);
+%             switch justify
+%                 case 'low'
+%                     cby=rectpos(2);
+%                 case 'midlo'
+%                     cby=rectpos(2)+(rectpos(4)-cbh)*0.25;
+%                 case 'mid'
+%                     cby=rectpos(2)+(rectpos(4)-cbh)*0.5;
+%                 case 'midhi'
+%                     cby=rectpos(2)+(rectpos(4)-cbh)*0.75;
+%                 case 'high'
+%                     cby=rectpos(2)+rectpos(4)-cbh;
+%             end
+%         case {'north','south'}
+%             cbw=dims(1)*rectpos(3);
+%             cbh=dims(2);
+%             switch justify
+%                 case 'low'
+%                     cbx=rectpos(1);
+%                 case 'midlo'
+%                     cbx=rectpos(1)+(rectpos(3)-cbw)*0.25;
+%                 case 'mid'
+%                     cbx=rectpos(1)+(rectpos(3)-cbw)*0.5;
+%                 case 'midhi'
+%                     cbx=rectpos(1)+(rectpos(3)-cbw)*0.75;
+%                 case 'high'
+%                     cbx=rectpos(1)+rectpos(3)-cbw;
+%             end
+%     end
+%     %unique features of each side: gap from axis
+%     switch placement
+%         case 'east'
+%             cbx=rectpos(1)+rectpos(3)+gap;
+%         case 'west'
+%             cbx=rectpos(1)-gap-cbw;
+%         case 'north'
+%             cby=rectpos(2)+rectpos(4)+gap;
+%         case 'south'
+%             cby=rectpos(2)-gap-cbh;
+%     end
+%     
+% 
+% %     [cbx,cby,cbw,cbh]
+%     hcb=colorbar(ax,placement);
+%     hcb.Position=[cbx,cby,cbw,cbh];
+% end
