4ee15b82f50e01ef6f4d8e785c0636bb0ebeae8b
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-10-12 18:42:49 -0400

    Update rename_clusters.m

--- rename_clusters.m
+++ rename_clusters.m
@@ -2,29 +2,32 @@ function [clustid, newids] = rename_clusters(old_clustid, newids, options)
 arguments
     old_clustid
     newids=[]
-    options.sortby='size'
+    options.sortby='none'
     options.datavals=[]
     options.sortdir='descend'
     options.do_numbers=false
     options.do_categorical=false
 end
 
-% if ~iscategorical(old_clustid)
-%     old_clustid=categorical(old_clustid);
-% end
-% oldids=categories(old_clustid);
-oldids=unique(old_clustid,'stable');
+if ~iscategorical(old_clustid)
+    old_clustid=categorical(old_clustid);
+end
+oldids=categories(old_clustid);
+% oldids=unique(old_clustid,'stable');
+ixs=1:length(old_clustid);
 
 datavals=options.datavals;
-switch options.sortby
-    case "size"
-        vals=arrayfun(@(x)nnz(old_clustid==x),oldids);
-    case "meanval"
-        vals=arrayfun(@(x)mean(datavals(old_clustid==x)),oldids);
-    case "medval"
-        vals=arrayfun(@(x)median(datavals(old_clustid==x)),oldids);
+if options.sortby~="none"
+    switch options.sortby
+        case "size"
+            vals=arrayfun(@(x)nnz(old_clustid==x),oldids);
+        case "meanval"
+            vals=arrayfun(@(x)mean(datavals(old_clustid==x)),oldids);
+        case "medval"
+            vals=arrayfun(@(x)median(datavals(old_clustid==x)),oldids);
+    end
+    [~,ixs]=sort(vals,options.sortdir);
 end
-[~,ixs]=sort(vals,options.sortdir);
 
 clustid=old_clustid;
 if isempty(newids)
