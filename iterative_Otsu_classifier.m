6c31ab315a3b44730d3cd0d20b187868f40eeccb
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-11-01 10:35:04 -0400

    update iterative classifiers

---
+++ iterative_Otsu_classifier.m
@@ -0,0 +1,155 @@
+function result=iterative_Otsu_classifier(tcounts, genes, markers, options)
+arguments
+    tcounts
+    genes
+    markers
+    options.blockvar=[]
+    options.ctrl_genes=50
+    options.thrmult=1
+    options.refine=true
+    options.nReps=1
+    options.mintop=50
+    options.fractop=.1
+    options.retain_markers=false
+
+    options.ref_subset=[]
+
+    options.summary=["min_cohen_d", "min_delta_prop"]
+    options.method="top";
+    options.ntop=10;
+    options.min_self_prop=0.1;
+end
+% Use gene scores + thresholding to call each cell type - a cell can be
+% assigned 0, 1, or many cell types.
+
+M=markers;
+CTs=unique(M.celltype,'stable')';
+nCells=size(tcounts,2);
+
+block=options.blockvar;
+if isempty(block)
+    block=true(size(tcounts,2),1);
+end
+block=categorical(block);
+blocks=categories(block);
+
+
+ref_subset=options.ref_subset;
+if isempty(ref_subset)
+    ref_subset=true(size(tcounts,2),1);
+end
+subix=find(ref_subset);
+
+if options.refine
+    options.nReps=max(2,options.nReps);
+end
+
+typelast=strings(nCells,1);
+Mlast=M;
+
+for r=1:options.nReps
+
+    % get marker scores
+    scores=zeros(length(CTs),nCells);
+    scorethr=false(length(CTs),nCells);
+    thr=zeros(length(CTs),1);
+    for j=1:length(blocks)
+        blocksub=block==blocks{j};
+        T=tcounts(:,blocksub);
+        for i=1:length(CTs)
+            thisCT=CTs(i);
+            mnames=M.gene(M.celltype==thisCT);
+            scores(i,blocksub)=score_genes(mnames, T, genes.name, ctrl_size=options.ctrl_genes);
+        end
+    end
+
+    % Identify top scoring cells per marker set
+    % - Otsu thresholds
+    for i=1:length(CTs)
+        this_score=scores(i,:);
+        thr(i,1)=geneThresholdOtsu(this_score)*options.thrmult;
+        scorethr(i,:)=this_score>thr(i,1);
+    end
+
+    % - alt: using correlation to reference built from topK scoring cells
+    
+    utype=sum(scorethr,1)==1;
+    type=strings(nCells,1);
+    CTrep=repmat(CTs,1,nCells);
+    uCTs=CTrep(scorethr(:,utype));
+    type(utype)=uCTs;
+    type(sum(scorethr,1)==0)="Unc";
+    type(sum(scorethr,1)>1)="Amb";
+    
+    ctplus=[CTs,"Unc","Amb"];
+    type=categorical(type,ctplus,ctplus);
+
+    summary(type')
+
+    if isequal(typelast,type)
+        break
+    end
+    typelast=type;
+
+    % refine markers
+    if options.refine && r<options.nReps
+        
+        TOP=[];
+        %fraction applied to each type, or total cells?
+%         Q = quantile(scores,1-options.fractop,2)
+        for i=1:length(CTs)
+%             topsel=scores(i,:)'>Q(i) & type==CTs{i};
+%             topix=find(topsel);
+%             topix=intersect(topix,subix);
+%             keepn=max(options.mintop, length(topix))
+%             TOP=[TOP;topix];
+            thistype=type==CTs{i} & ref_subset;
+            keepn=max(options.mintop, round(options.fractop*nnz(thistype)));
+            [~, topix]=maxk(scores(i,ref_subset),keepn,2);
+            TOP=[TOP,topix];
+        end
+        TOP=unique(TOP);
+        keep=ismember(1:length(type),TOP);
+        
+        typetop=removecats(type,["Amb","Unc"]);
+        typetop(~keep)=missing(); 
+        unc=find(type=="Unc"); 
+        keepn=max(options.mintop, round(options.fractop*nnz(type=="Unc")));
+        kix=randi(length(unc),keepn,1);
+        typetop(unc(kix))="Unc";
+        typetop=removecats(typetop);
+
+        % get the new marker list based on top scoring cells
+        ges=GroupedExpressionSummary(genes,tcounts,typetop,block);
+        ges.computeEffectSizes();
+
+        M=table;
+        for i=1:length(options.summary)
+            M=[M;ges.topMarkers(options.summary(i),options.method, CTs, CTs, ...
+                  nTop=options.ntop, min_self_prop=options.min_self_prop)];
+        end
+        M=M(:,1:2);
+        if options.retain_markers
+            M=[M;Mlast];
+%             M=[M;markers(ismember(markers.celltype,CTs),:)];
+        end
+        M=unique(M,'rows','stable');
+        M=sortrows(M,["celltype","gene"]);
+
+%         multimarkers? genes listed for more than one cell type...
+%         multimarkers=length(M.gene)-length(unique(M.gene));
+
+%         equalM=isequal(Mlast,M)
+        summary(categorical(M.celltype,CTs,CTs)')
+
+        Mlast=M;
+    end
+end
+
+result = options;
+result.type = type;
+result.scores = scores;
+result.thr = thr;
+result.markers = M;
+result.CTs = CTs;
+result.typetop = typetop;
--- iterative_corr_classifier.m
+++ iterative_corr_classifier.m
@@ -1,28 +1,46 @@
-function [type, scores, thr, M, CTs, typetop]=iterative_classifier(tcounts, genes, markers, options, markeropts)
+function result=iterative_corr_classifier(tcounts, genes, markers, options, options)
 arguments
     tcounts
     genes
     markers
-    options.blockvar=ones(1,size(tcounts,2))
-    options.thrsub=true(1,size(tcounts,2))
-    options.thrmult=1
+    options.blockvar=[]
+    options.ctrl_genes=50
     options.refine=true
     options.nReps=1
     options.mintop=50
     options.fractop=.1
     options.retain_markers=false
 
-    markeropts.summary=["min_cohen_d", "min_delta_prop"]
-    markeropts.method="top";
-    markeropts.ntop=10;
-    markeropts.min_self_prop=0.1;
+    options.summary=["min_cohen_d", "min_delta_prop"]
+    options.method="top";
+    options.ntop=10;
+    options.min_self_prop=0.1;
 end
+% SingleR approach
+% - pool top scoring cells to define a "reference" cell
+% - compute the Spearman correlation between its expression profile and
+% that of each cell type reference This gives a correlation coefficient to
+% each cell type for each cell (also p-val). Assign to 0, 1, or more cell
+% types based on that.
+% - need a way to initialize references: marker gene score topk?
+%
+% scores:
+% - per-label score is a fixed quantile (by default, 0.8) of the correlations across all samples with that label
+% - benefit: scores are comparable across cell types (in [0,1]).
+%
+% diagnostics:
+% - deltas - difference between the score for the assigned label and the median across all labels for each cell
+
 
 M=markers;
 CTs=unique(M.celltype,'stable')';
 nCells=size(tcounts,2);
 
-block=categorical(options.blockvar);
+block=options.blockvar;
+if isempty(block)
+    block=ones(1,size(tcounts,2));
+end
+block=categorical(block);
 blocks=categories(block);
 
 if options.refine
@@ -44,16 +62,20 @@ for r=1:options.nReps
         for i=1:length(CTs)
             thisCT=CTs(i);
             mnames=M.gene(M.celltype==thisCT);
-            scores(i,blocksub)=score_genes(mnames, T, genes.name, ctrl_size=50);
+            scores(i,blocksub)=score_genes(mnames, T, genes.name, ctrl_size=options.ctrl_genes);
         end
     end
 
-    %get thresholds
+    % Identify top scoring cells per marker set
+
+    
     for i=1:length(CTs)
         this_score=scores(i,:);
-        thr(i,1)=geneThresholdOtsu(this_score(options.thrsub))*options.thrmult;
+        thr(i,1)=geneThresholdOtsu(this_score)*options.thrmult;
         scorethr(i,:)=this_score>thr(i,1);
     end
+
+    % - alt: using correlation to reference built from topK scoring cells
     
     utype=sum(scorethr,1)==1;
     type=strings(nCells,1);
@@ -93,16 +115,14 @@ for r=1:options.nReps
         typetop(unc(kix))="Unc";
         typetop=removecats(typetop);
 
+        % get the new marker list based on top scoring cells
         ges=GroupedExpressionSummary(genes,tcounts,typetop,block);
         ges.computeEffectSizes();
 
-
         M=table;
-        for i=1:length(markeropts.summary)
-            M=[M;ges.topMarkers(markeropts.summary(i),markeropts.method, CTs, ...
-                nTop=markeropts.ntop, min_self_prop=markeropts.min_self_prop)];
-%             M=[M;ges.topMarkers(markeropts.summary(i),markeropts.method,, ...
-%                 nTop=markeropts.ntop, min_self_prop=markeropts.min_self_prop)];
+        for i=1:length(options.summary)
+            M=[M;ges.topMarkers(options.summary(i),options.method, CTs, CTs, ...
+                  nTop=options.ntop, min_self_prop=options.min_self_prop)];
         end
         M=M(:,1:2);
         if options.retain_markers
@@ -112,9 +132,21 @@ for r=1:options.nReps
         M=unique(M,'rows','stable');
         M=sortrows(M,["celltype","gene"]);
 
+%         multimarkers? genes listed for more than one cell type...
+%         multimarkers=length(M.gene)-length(unique(M.gene));
+
 %         equalM=isequal(Mlast,M)
         summary(categorical(M.celltype,CTs,CTs)')
 
         Mlast=M;
     end
 end
+
+result = options;
+result.type = type;
+result.scores = scores;
+result.thr = thr;
+result.markers = M;
+result.CTs = CTs;
+result.typetop = typetop;
+result.refs = refs;
