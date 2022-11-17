3e1f96b6d3d4b0dd2757c80f4b18d921a9748954
Patrick Allen Fletcher <patrick.allen.fletcher@gmail.com>
2022-11-17 15:24:57 -0500

    Update heatmap_plot.m

--- heatmap_plot.m
+++ heatmap_plot.m
@@ -18,3 +18,50 @@ arguments
     opts.cbJust {mustBeMember(opts.cbJust,["low","midlo","mid","midhi","high"])}='mid'
     opts.cbDigits=1
 end
+
+%indicate ids using scatter with  'Clipping', 'off'?
+
+l=opts.margins(1);
+b=opts.margins(1);
+r=opts.margins(1);
+t=opts.margins(1);
+
+ax=tight_subplot(1,1,1,0,[b,t],[l,r]);
+
+% need an attached mini-axis for cell annotation
+% ax=tight_subplot(1,1,1,0,[b,t],[l,r]);
+% imagesc(cg.clust.clusterID(1:10:end,1)')
+% colormap(opts.gcols)
+
+IM=vals;
+IM=smoothdata(IM,2,"loess",100);
+IM(IM<0)=0;
+
+IM=normalize(IM,2,'range');
+
+% clustering genes
+IMs=smoothdata(IM,2,"loess",500);
+D=pdist(IMs,'euclidean');
+Z=linkage(D,"ward");
+o=optimalleaforder(Z,D,Criteria="group");
+
+%order by peak
+% [~,pix]=max(IMs,[],2);
+% [~,o]=sort(pts(pix));
+
+IM=IM(o,:);
+glabs="\it"+glist(o);
+
+
+imagesc(ax,pts,1:length(glist),IM)
+
+% axis("tight")
+yticks(1:length(glabs))
+yticklabels(glabs)
+
+if isdiverging
+    cmap=split_cmap;
+else
+    cmap=cbrewer('seq','Reds',256); cmap(1,:)=[1,1,1];%cmap(1,:)=rgb('lightgray');
+end
+colormap(cmap)
