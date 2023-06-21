6e317ba8b8174bd2b5e9b37a96b9e16e1ba9dad3
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2023-06-21 11:09:07 -0400

    Update pyGAMfit.m

--- pyGAMfit.m
+++ pyGAMfit.m
@@ -1,19 +1,30 @@
-function gam = pyGAMfit(X,y,sopts,options)
+function model = pyGAMfit(X,y,sopts,modelopts,options)
 arguments
     X
     y
     sopts.n_splines=20
     sopts.spline_order=3
     sopts.lam=0.6
-    sopts.penalties='auto'
+    sopts.penalties='auto' % ["derivative", "l2"]
     sopts.basis='ps' %or 'cp' for periodic
     sopts.edge_knots=string(missing) %aka None in python
-    options.fit_intercept = true
-    options.max_iter=100
-    options.tol=1e-4
-    options.verbose=false
+    modelopts.fit_intercept = true
+    modelopts.max_iter=200
+    modelopts.tol=1e-4
+    modelopts.verbose=false
+    options.gridLam = []
+    options.gridKnots = []
+    options.model = "LinearGAM"
 end
 pygam=py.importlib.import_module('pygam.pygam');
+
+% ? auto ==   penalties=["derivative", "l2"],
+% cellrank: link log dist gamma
+
+% weights? CR uses fate probabilities
+% stats? for discovery of traj-assoc. genes. 
+
+%cluster the fits (genes are obs.)
 
 % Sline term
 %pygam.terms.s(feature, n_splines=20, spline_order=3, lam=0.6,
@@ -22,7 +33,7 @@ pygam=py.importlib.import_module('pygam.pygam');
 
 sopts.n_splines=int32(sopts.n_splines);
 sopts.spline_order=int32(sopts.spline_order);
-
+sopts.penalties=cellstr(sopts.penalties);
 if isnumeric(sopts.edge_knots)
     sopts.edge_knots=py.numpy.array(sopts.edge_knots,'float32',pyargs('order','C'));
 end
@@ -32,31 +43,58 @@ s=pygam.SplineTerm(int32(0),...
                    spline_order=sopts.spline_order, ...
                    lam=sopts.lam, ...
                    basis=sopts.basis, ...
+                   penalties = sopts.penalties, ...
                    edge_knots=sopts.edge_knots);
 
 %classpygam.pygam.LinearGAM(terms='auto', max_iter=100, tol=0.0001,
 %scale=None, callbacks=['deviance', 'diffs'], fit_intercept=True,
 %verbose=False, **kwargs)
 
-% args = pyargs('terms',s, ...
-%     'fit_intercept',options.fit_intercept, ...
-%     'max_iter',int32(options.max_iter), ...
-%     'tol',options.tol, ...
-%     'verbose',options.verbose);
+kwargs = pyargs('terms',s, ...
+    'fit_intercept',modelopts.fit_intercept, ...
+    'max_iter',int32(modelopts.max_iter), ...
+    'tol',modelopts.tol, ...
+    'verbose',modelopts.verbose);
 
-% gam = pygam.LinearGAM(args);
-
-options.max_iter=int32(options.max_iter);
-% gam = pygam.LinearGAM(s, options);
-
-gam = pygam.LinearGAM(terms = s, ...
-                      fit_intercept=options.fit_intercept, ...
-                      max_iter=options.max_iter, ...
-                      tol=options.tol, ...
-                      verbose=options.verbose);
-
+switch options.model
+    case "GAM"
+        model = pygam.GAM(kwargs);
+    case "GammaGAM"
+        model = pygam.GammaGAM(kwargs);
+    case "InvGaussGAM"
+    case "LinearGAM"
+        model = pygam.LinearGAM(kwargs);
+    case "LogisticGAM"
+        model = pygam.LogisticGAM(kwargs);
+    case "PoissonGAM"
+        model = pygam.PoissonGAM(kwargs);
+    case "ExpectileGAM"
+        model = pygam.ExpectileGAM(kwargs);
+end
 
 X = py.numpy.array(X,'float32',pyargs('order','C'));
 y = py.numpy.array(y,'float32',pyargs('order','C'));
 
-gam = gam.fit(X, y);
+model = model.fit(X, y);
+
+%disgusting:
+doGridL = false;
+doGridK = false;
+if ~isempty(options.gridLam)
+    doGridL = true;
+    options.gridLam = py.numpy.array(options.gridLam);
+end
+if ~isempty(options.gridKnots)
+    doGridK = true;
+    options.gridKnots = int32(options.gridKnots);
+end
+
+if doGridL&&~doGridK
+    model.gridsearch(X,y,progress=modelopts.verbose, lam=options.gridLam);
+end
+if ~doGridL&&doGridK
+    model.gridsearch(X,y,progress=modelopts.verbose,n_splines=options.gridKnots);
+end
+if doGridL&&doGridK
+    model.gridsearch(X,y,progress=modelopts.verbose,lam=options.gridLam, n_splines=options.gridKnots);
+end
