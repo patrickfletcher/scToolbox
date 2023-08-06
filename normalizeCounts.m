ec2762eb392b8adfba0f29b04901ea2cceacb7f1
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2023-08-06 00:52:51 -0400

    fix so external SFSs give correct final scale

--- normalizeCounts.m
+++ normalizeCounts.m
@@ -1,60 +1,48 @@
 function [ncounts,sfs]=normalizeCounts(counts, options)
 arguments
     counts
+    options.scale = []
+    options.center_measure = "median"
     options.sizefactors=[]
-    options.scale="median"
-    options.scale_value = 1e4
-    options.max_frac=[]
 end
 %normalize counts
 
-%TODO: center size factors around 1??
+%TODO
+% - batch handling: multibatchnorm?
+% - exclude genes making up >max_frac of a cell's expression? scanpy [e.g. poor quality cells/RBCs?]
+% - subset of genes used to compute trend?
 
-%TODO: other methods???
-% - what if cell borrowed info from k-nn? needs way to get KNN (pca???)
-% -- 1. naive counts_per_cell, naive scale --> sfs0=counts/median(counts)
-%  [knn: sfs0 -> norm, logcounts -> pca -> knn.idx]
-% -- 2. summed counts_per_cell: countsK, summed scale=median(countsK): scaleK --> sfsK=countsK/scaleK 
-% -- 3. sfs --> sfsK*
-
-%TODO: params.method, etc. 
-
-scale=options.scale;
-max_frac=options.max_frac;
-
-exclude_hiexp=~isempty(max_frac);
+% could support others... https://en.wikipedia.org/wiki/Central_tendency 
+switch options.center_measure
+    case "mean"
+        center_measure = @mean;
+    case "median"
+        center_measure = @median;
+end
 
 %sum counts per cell
 counts_per_cell=full(sum(counts,1));
 
-%scale computed before exclude genes
-switch scale
-    case "median"
-        scale=median(counts_per_cell);
-    case "mean"
-        scale=mean(counts_per_cell);
-    case "value"
-        scale = options.scale_value;
+% rescale the normalized counts to a) original scale (measure?) or b) a specified target scale
+scale=options.scale;
+if isempty(scale) %sf_center_target = 1 gives original scale using center_measure
+    scale = center_measure(counts_per_cell);
 end
 
-if exclude_hiexp
-    %redo the counts_per_cell
-    high_frac=full(sum(counts>max_frac*counts_per_cell,2));
-    counts_per_cell=full(sum(counts(~high_frac,:),1));
-    scale=median(counts_per_cell);
-end
-
-%size factors
+%basic size factor: divide by counts_per_cell --> fraction of cell total counts per gene
 sfs=options.sizefactors;
 if isempty(sfs)
     sfs=counts_per_cell/scale;
+else
+    % adjust user sfs to get correct scale when using "center_measure"
+    % - for original data scale: sf_center_target = 1 (center the sfs at 1)
+    % - for specified scale: sf_center_target = center_measure(counts_per_cell)/scale
+    sf_center_target = center_measure(counts_per_cell)/scale;
+    sf_center = center_measure(sfs);
+    sfs = sfs/sf_center * sf_center_target;
 end
+
 sfs=sfs(:)';
-
-% %alt: DESeq - do I need to geomean only positive vals to avoid divbyzeros?
-% gm=geomean(counts,2); %pseudoreference sample (cell)
-% sfs=median(counts./gm,1);
-
 if issparse(counts)
 %     A=spfun(@(x)1./x,sfs);
     [i,j,a]=find(counts);
@@ -67,3 +55,55 @@ if issparse(counts)
 else
     ncounts=counts./sfs;
 end
+
+% verify
+% scalef = center_measure(full(sum(ncounts,1)));
+% [scale,scalef]
+% (scalef-scale)/scale*100 %relative error
+
+
+
+
+%???
+
+    % options.min_mean = 0.1
+    % options.batch = []
+    % options.max_frac=[]
+
+% max_frac=options.max_frac;
+% if ~isempty(max_frac)
+%     %redo the counts_per_cell
+%     high_frac=full(sum(counts>max_frac*counts_per_cell,2));
+%     counts_per_cell=full(sum(counts(~high_frac,:),1));
+%     scale=median(counts_per_cell);
+% end
+
+% batch = options.batch;
+% if isempty(batch)
+%     batch = ones(1,size(counts,2));
+% end
+% batch = findgroups(batch);
+
+% gsub = true(size(counts,1),1);
+% if ~isempty(options.min_mean)
+%     gsub = 
+% end
+
+% scaleb = splitapply(@median, counts_per_cell(:), batch(:));
+
+    % %should do this per batch? find the batch with mean sfs=1?
+    % sfs_b = splitapply(@mean, sfs(:), batch(:));
+    % [~,ix]=min(abs(sfs_b-1));
+    % old_scale = scale_b(ix);
+    % sfs = sfs.*old_scale./scale;
+    
+% %batch: multibatchnorm.
+% % 1. compute sfs=median(counts_per_cell) for all. (libSizefactors - above)
+% % 2. for each batch: sf / mean(sf), mean(counts/sf)
+% % 3. using only genes mean_ncounts>min_mean, find smallest sf. Shrink sizefactors of all batches to match the smallest.
+% if ~isempty(options.batch)
+%     b = findgroups(options.batch);
+%     sfs_bm = splitapply(@mean, sfs, batch);
+%     sfs_ratio = splitapply(@(x){x./mean(x)},sfs,batch); 
+%     sfs_ratio=cat(2,sfs_ratio{:});
+% end
