19d70d0f5646fe77e1882cd2936a0d5a4cda493a
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2023-10-25 23:18:14 -0400

    Update neighbors.m

--- neighbors.m
+++ neighbors.m
@@ -2,7 +2,7 @@ function result = neighbors(X, params, gparams, options)
 arguments
     X
     params.n_neighbors=30
-    params.metric="correlation"
+    params.metric="euclidean"
     gparams.graph_type{mustBeMember(gparams.graph_type,["knn","snn"])}="knn"
     gparams.graph_weights = "adj"
     gparams.pruneThr = 0
@@ -53,8 +53,16 @@ result.dists=dists;
 %graph representation
 % TODO: split out graph building stuff into separate function?
 if options.makeGraph
+    disp('Building neighbor graph...')
+    tic
+
+    small_val = 1e-6;
+
     sources = repelem(1:m, k)';
     targets = reshape(idx', [], 1);
+
+    A = sparse(sources, targets, ones(size(sources)), m, m);
+    S = A'*A;
 
     if gparams.graph_type=="snn" || gparams.graph_weights=="jaccard" || gparams.graph_weights=="number"
         [shared_neighbors, jaccard_index] = get_jaccard();
@@ -78,7 +86,7 @@ if options.makeGraph
 
         case "dist" %reciprocal? 1/dist?  exponential kernel???
             weights = reshape(1./dists', 1, []);
-            weights(isinf(weights))=0;
+            weights(isinf(weights)) = 1; %these are self-distances
     end
 
     G = sparse(sources, targets, weights, m, m);
@@ -90,24 +98,38 @@ if options.makeGraph
         case "snn" 
             discards = jaccard_index<gparams.pruneThr;
     end
-    G(discards) = 0;
+    G(discards) = small_val;
 
     %symmetrize? Louvain non-oriented does this anyway.
+    % TODO: something is very slow!!!!!
     if gparams.symmetrize
+        G = full(G);
         G = (G + G')/2;
+        G = sparse(G);
     end
 
     result.graph=G;
+
+    disp("graph time: " + num2str(toc) + "s")
 end
 
     %TODO: speed of looping? outer loop is on columns, cellfun loops on rows...
+    % time scales with: m, k
+    % --> use hashing? categorical
+    % --> one-hot? binary m x m matrix
     function [shared_neighbors, jaccard_index] = get_jaccard()
         %similarity = Jaccard index (shared neighbors) -> what fraction of my neighbors include me as one of theirs?         
         shared_neighbors = zeros(size(idx));
         for i=1:k
-            n_of_n=idx(idx(:,i),:);
-            this_intersect = cellfun(@(c,d) length(intersect(c,d)), num2cell(idx, 2), num2cell(n_of_n, 2));
-            shared_neighbors(:,i) = this_intersect;
+            this_idx = idx(:,i);
+            n_of_n=idx(this_idx,:);
+            s = repelem(this_idx, k)';
+            t = reshape(n_of_n', [], 1);
+            v=true(size(t));
+            is_neighbor = sparse(s, t, v, m, m);
+
+            % this_intersect = cellfun(@(c,d) length(intersect(c,d)), num2cell(idx, 2), num2cell(n_of_n, 2));
+            % shared_neighbors(:,i) = this_intersect;
         end
         shared_neighbors = reshape(shared_neighbors', [], 1);
         jaccard_index = shared_neighbors ./ (2*k - shared_neighbors); % <--- union = size(a) + size(b) - intersection
